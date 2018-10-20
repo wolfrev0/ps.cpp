d2665d6858d95552e5fe96e3e96e1b3cc03eafe7
Lobo Prix <lobo_prix@naver.com>
2018-10-21 03:05:24 +0900

    Vec2 Refactoring

--- Project1/Vec2.h
+++ Project1/Vec2.h
@@ -17,15 +17,17 @@ struct Vec2
 	inline Vec2 operator-()const { return Vec2(-x, -y); }
 	inline Vec2 operator- (const Vec2 &r)const { return *this + -r; }
 	inline Vec2 operator* (T r)const { return Vec2(x*r, y*r); }
+	inline Vec2 operator+= (const Vec2 &r) { return *this = *this + r; }
+	inline Vec2 operator-= (const Vec2 &r) { return *this = *this - r; }
+	inline Vec2 operator*= (T r) { return *this = *this * r; }
 	inline T size()const { return hypot(x, y); }
 	inline T sizesq()const { return dot(*this); }
 	inline Vec2 normalize()const { return Vec2(x / size(), y / size()); }
 	inline T dot(const Vec2 &r) const { return x * r.x + y * r.y; }
+	inline T dot(const Vec2 &a, const Vec2 &b) const { return (a - *this).dot(b - *this); }
 	inline T cross(const Vec2 &r)const { return x * r.y - y * r.x; }
-	inline T ccw(const Vec2 &a, const Vec2 &b) const { return (a - *this).cross(b - *this); }
+	inline T cross(const Vec2 &a, const Vec2 &b) const { return (a - *this).cross(b - *this); }
+	inline T ccw(const Vec2 &a, const Vec2 &b) const { return cross(a, b); }
 	inline T angle()const { return atan2(y, x); }
 	inline Vec2 project(const Vec2 &p)const { Vec2 base = normalize(); return base * base.dot(p); }
-	inline Vec2 operator+= (const Vec2 &r) { return *this = *this + r; }
-	inline Vec2 operator-= (const Vec2 &r) { return *this = *this - r; }
-	inline Vec2 operator*= (T r) { return *this = *this * r; }
-} zero, err = Vec2(inf<Vec2::T>(), inf<Vec2::T>());
+} zero, err = Vec2(inf<Vec2::T>(), inf<Vec2::T>()), epsv = Vec2(eps, eps);
