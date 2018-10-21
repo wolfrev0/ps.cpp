b1d605d3525acf02a0e0bb9a35bdbed33e9bd92d
Lobo Prix <lobo_prix@naver.com>
2018-10-21 17:32:36 +0900

    NumberTheory Refactoring

---
+++ Project1/ModNum.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Core.h"
+
+template<int m>
+struct ModNum
+{
+	using T = ll;
+	ModNum(T n = 0) :n((n%m + m) % m) {}
+	//operator T() const { return n; }
+	inline T val()const { return n; }
+	inline ModNum mulid() const { return 1; }
+
+	inline ModNum operator - () const { return -n; }
+	inline ModNum operator + (const ModNum b)const { return n + b.val(); }
+	inline ModNum operator - (const ModNum b)const { return n - b.val() + m; }
+	inline ModNum operator * (const ModNum b)const { return n * b.val(); }
+	inline ModNum operator / (const ModNum b)const { return n * fastpow(ModNum(m, b.val()), m - 2).num; }
+	inline ModNum operator+= (const ModNum b) { return *this = *this + b.val(); }
+	inline ModNum operator-= (const ModNum b) { return *this = *this - b.val(); }
+	inline ModNum operator*= (const ModNum b) { return *this = *this * b.val(); }
+	inline ModNum operator/= (const ModNum b) { return *this = *this / b.val(); }
+
+	inline ModNum &operator++ () { *this += 1; return *this; }
+	inline ModNum &operator-- () { *this -= 1; return *this; }
+	inline ModNum operator++(int) { auto ret = *this; ++*this; return ret; }
+	inline ModNum operator--(int) { auto ret = *this; --*this; return ret; }
+
+	inline bool operator==(const ModNum &r) const { return n == r.n; }
+	inline bool operator!=(const ModNum &r) const { return !(*this == r); }
+	inline bool operator<(const ModNum &r) const { return n < r.n; }
+	inline bool operator<=(const ModNum &r) const { return n <= r.n; }
+	inline bool operator>(const ModNum &r) const { return n > r.n; }
+	inline bool operator>=(const ModNum &r) const { return n >= r.n; }
+
+	inline ModNum &operator= (const ModNum &r) { n = r.n; return *this; }
+private:
+	T n;
+};
