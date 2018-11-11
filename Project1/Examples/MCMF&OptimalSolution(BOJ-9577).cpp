2837bd50ed51b35868e8efd92ad40138df0d91a8
Lobo Prix <lobo_prix@naver.com>
2018-11-12 02:18:35 +0900

    Update Project1/Examples/MCMF&OptimalSolution(BOJ-9577).cpp

---
+++ Project1/Examples/MCMF&OptimalSolution(BOJ-9577).cpp
@@ -0,0 +1,47 @@
+#include "Graph.h"
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(10);
+	srand((uint)time(0));
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+		MCMF g(200);
+		forh(i, 0, m)
+		{
+			int s, e, a;
+			cin >> s >> e>>a;
+			forh(j, 0, a)
+			{
+				int q;
+				cin >> q;
+				q--;
+				forh(k, s, e)
+					g.add_edge_mcmf(k, 100 + q, 1, k*k*k);
+			}
+		}
+		forh(i, 0, 100)
+		{
+			g.add_edge_mcmf(g.src, i, 1, 0);
+			g.add_edge_mcmf(100 + i, g.snk, 1, 0);
+		}
+
+		if (g.mcmf().second != n)
+			cout << -1 << endl;
+		else
+		{
+			int ans = 0;
+			forh(i, 0, 100)
+				for (auto j : g.g[i])
+					if (j.v < 200 && !j.w.cap)
+						ans = i + 1;
+			cout << ans << endl;
+		}
+	}
+
+	return 0;
+}
