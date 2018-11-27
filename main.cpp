c1cd4f230492f44ea18045cbc202bf3a21aa628b
Lobo Prix <lobo_prix@naver.com>
2018-11-27 15:00:18 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "Core.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     cout<<"123f"<<endl;
+
+    forh(i, 0, 10){
+        cout<<i*i<<endl;
+    }
+
     return 0;
 }
