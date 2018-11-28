2d3febcec3bf26b17513a0b4ef1071b77cbbc6d5
Lobo Prix <lobo_prix@naver.com>
2018-11-28 10:50:41 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include "Core.h"
-#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    cout<<"123f"<<endl;
-
-    forh(i, 0, 10){
-        cout<<i*i<<endl;
-    }
+    cout<<"Hello"<<endl;
 
     return 0;
 }
