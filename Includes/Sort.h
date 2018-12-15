c2b28e518a0ec854b6e7f356c6aec0fca0080330
Lobo Prix <lobo_prix@naver.com>
2018-12-15 09:50:55 +0900

    Sort

---
+++ Includes/Sort.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Core.h"
+
+template<typename T>
+inline void counting_sort(vector<T>& v, const function<T(T)> &f = [](const T& x){return x;})
+{
+	const int MAX_BW = 500001;
+	static vector<T> bucket[MAX_BW+1];
+	for(auto &i:bucket)
+		i.clear();
+
+    T m = inf<T>(), M = -inf<T>();
+    for(auto &i:v){
+        m = min(m, f(i));
+        M = max(M, f(i));
+    }
+
+    for(auto i:v)
+        bucket[f(i) - m].push_back(i);
+    v.clear();
+    forc(i, 0, M-m)
+        for(auto j:bucket[i])
+            v.push_back(j);
+}
