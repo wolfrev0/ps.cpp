d28e1c4b43746ceeebfb36e867c3849c30495f5a
Lobo Prix <lobo_prix@naver.com>
2018-12-15 10:46:16 +0900

    Update Frac.h

--- Includes/Frac.h
+++ Includes/Frac.h
@@ -4,12 +4,25 @@
 struct Frac
 {
 	ll a, b;
-	Frac(ll a, ll b=1):a(b?a:1),b(b){}
-	bool operator<(const Frac& r)const { return b<0 ^ r.b<0 ? a * r.b > r.a*b : a*r.b < r.a*b; }
-	bool operator==(const Frac& r)const { return a * r.b == r.a*b; }
-	Frac operator*(const Frac& r)const { return {a*r.a, b*r.b}; }
-	Frac inv()const{return {b,a};}
-	bool is_inf()const{return a&&!b;}
-	bool is_nan()const{return !a&&!b;}
-	ld to_ld(){return (ld)a/b;}
+	Frac(ll a, ll b=1):a(a),b(b){normalize();}
+	inline ull hash()const{return (ull)a*prime+(ull)b;}
+	inline bool operator<(const Frac& r)const { return (b<0) ^ (r.b<0) ? a * r.b > r.a*b : a*r.b < r.a*b; }
+	inline bool operator==(const Frac& r)const { return a * r.b == r.a*b; }
+	inline Frac operator*(const Frac& r)const { return {a*r.a, b*r.b};}
+	inline Frac inv()const{return {b,a};}
+	inline bool is_inf()const{return a&&!b;}
+	inline bool is_nan()const{return !a&&!b;}
+	inline ld to_ld(){return (ld)a/b;}
+	inline void normalize(){
+		if(is_nan())
+			return;
+		if(is_inf()){
+			a=1;
+			return;
+		}
+		// ll g = gcd(a,b); 
+		// a/=g; b/=g;
+		// if(b<0)
+		// 	a*=-1, b*=-1;
+	}
 };
