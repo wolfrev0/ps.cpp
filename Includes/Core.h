d63df6c783ce8b9f3c2e9d0f735ec4ab5a6e1ea0
Lobo Prix <lobo_prix@naver.com>
2019-01-22 00:01:41 +0900

    for series safty update

--- Includes/Core.h
+++ Includes/Core.h
@@ -14,16 +14,16 @@ const f64 pi=acos(-1);
 const f64 eps=1e-12;
 i32 mod=i32(1e9 + 7);
 #define endl '\n'
-#define forh(var, begin, end) for(auto var=begin; begin<=var && var<end; ++var)//for: half-opened range
-#define forhi(var, begin, end) for(auto var=end-1; begin<=var && var<end; --var)//inversion
-#define forho(var, begin, end) auto var=begin; for(; begin<=var && var<end; ++var)//out declaration
-#define forhoi(var, begin, end) auto var=end-1; for(; begin<=var && var<end; --var)
-#define forc(var, begin, end) for(auto var=begin; begin<=var && var<=end; ++var)//for: closed range
-#define forci(var, begin, end) for(auto var=end; begin<=var && var<=end; --var)//inversion
-#define forco(var, begin, end) auto var=begin; for(; begin<=var && var<=end; ++var)//out declaration
-#define forcoi(var, begin, end) auto var=end; for(; begin<=var && var<=end; --var)//inversion
-#define trav(var, begin, cond) for(auto var=begin; cond; ++var)//travel
-#define travo(var, begin, cond) auto var=begin; for(; cond; ++var)//out declaration
+#define forh(var, begin, end) for(int var=begin; (int)begin<=var && var<(int)end; ++var)//for: half-opened range
+#define forhi(var, begin, end) for(int var=end-1; (int)begin<=var && var<(int)end; --var)//inversion
+#define forho(var, begin, end) int var=begin; for(; (int)begin<=var && var<(int)end; ++var)//out declaration
+#define forhoi(var, begin, end) int var=end-1; for(; (int)begin<=var && var<(int)end; --var)
+#define forc(var, begin, end) for(int var=begin; (int)begin<=var && var<=(int)end; ++var)//for: closed range
+#define forci(var, begin, end) for(int var=end; (int)begin<=var && var<=(int)end; --var)//inversion
+#define forco(var, begin, end) int var=begin; for(; (int)begin<=var && var<=(int)end; ++var)//out declaration
+#define forcoi(var, begin, end) int var=end; for(; (int)begin<=var && var<=(int)end; --var)//inversion
+#define trav(var, begin, cond) for(int var=begin; cond; ++var)//travel
+#define travo(var, begin, cond) int var=begin; for(; cond; ++var)//out declaration
 
 template<typename T>
 struct Regular{
@@ -74,16 +74,16 @@ struct Regular<f128>{
 		static f128 inf(){ return numeric_limits<f64>::max()/2; }
 };
 using I128=Regular<i128>; using U128=Regular<u128>; using F128=Regular<f128>;
-ostream& operator<<(ostream &os, U128 n){
-	string s;while(n){ s.push_back(n%10+'0');n.n/=10; }
+ostream& operator<<(ostream &os, u128 n){
+	string s;while(n){ s.push_back(n%10+'0');n/=10; }
 	reverse(s.begin(), s.end());
 	return os<<s;
 }
-ostream& operator<<(ostream &os, I128 n){
-  if(n<0){ os<<'-';n.n*=-1; }
-	return os<<(U128)n.n;
+ostream& operator<<(ostream &os, i128 n){
+  if(n<0){ os<<'-';n*=-1; }
+	return os<<(u128)n;
 }
-ostream& operator<<(ostream &os, F128 n){return os<<f64(n.n);}
+ostream& operator<<(ostream &os, f128 n){return os<<f64(n);}
 #endif
 
 //Extensions
