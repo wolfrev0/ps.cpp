22bc84ff0378f605e821a0de102c16f510301563
LoboPrix <lobo_prix@naver.com>
2019-01-28 04:06:33 +0900

    Splay Tree Semi-Complete
    Base0, [l,r]
    todo: make range [s, e)

--- Includes/SplayTree.h
+++ Includes/SplayTree.h
@@ -25,9 +25,9 @@ struct SplayTree{
 
     Node* nth(int n){
       int lsz = l?l->size:0;
-      if(lsz>n)
+      if(lsz>n+1)
         return l->nth(n);
-      if(lsz<n)
+      if(lsz<n+1)
         return r->nth(n-lsz-1);
       return this;
     }
@@ -38,10 +38,11 @@ struct SplayTree{
   SplayTree(int n):n(n){
     Node* x = new Node();
     root=x;
-    x->size = n;
-    forh(i, 1, n){
+    //+2 is Left(-1) Right(n) mock nodes
+    x->size = n+2;
+    forh(i, 1, n+2){
       x->adoptR(new Node());
-      x->r->size = n - i;
+      x->r->size = n+2 - i;
       x=x->r;
     }
   }
@@ -104,105 +105,3 @@ struct SplayTree{
     return root->r->l->acc;
   }
 };
-
-// struct SplayTree2{
-//   struct Node{
-//     static Node* null;
-//     static Node* create(){
-//       if(!null){
-//         null=new Node();
-//         null->p=null->l=null->r=null;
-//       }
-//       auto ret = new Node();
-//       ret->p=ret->l=ret->r=null;
-//       return ret;
-//     }
-//     Node *p, *l, *r;
-//     i64 val=0, acc=0, size=0;
-
-//     //move to position of root
-//     void splay(Node*& root){
-//       while(p != Node::null){
-//         ((this==p->l)==(p==p->p->l)?p:this)->rotate();
-//         this->rotate();
-//       }
-//       root=this;
-//     }
-
-//     Node* nth(i64 ord){
-//       if(size-1-r->size > ord)
-//         return l->nth(ord);
-//       else if(size-1-r->size<ord)
-//         return r->nth(ord);
-//       return this;
-//     }
-//   private:
-//     Node(){}
-
-//     //move to parent pos
-//     void rotate(){
-//       if(this->p==null)
-//         return;
-//       Node* m;
-//       function<Node*&(Node*)> a=[](Node* x)->Node*&{return x->l;};
-//       function<Node*&(Node*)> b=[](Node *x)->Node*&{return x->r;};
-//       if(this==p->r)
-//         swap(a,b);
-      
-//       m=a(p)=b(this);
-//       b(this)=p;
-
-//       auto psav=p;
-//       p=p->p;
-//       psav->p=this;
-//       if(m!=Node::null)
-//         m->p=p;
-//       a(p)=this;
-//       psav->renew();
-//       this->renew();
-//     }
-
-//     void renew(){
-//       size = 1+l->size+r->size;
-//       acc = val+l->acc+r->acc;
-//     }
-//   };
-
-//   Node* root;
-
-//   SplayTree(i64 n, i64 id=0, i64 lazyid=0){
-//     auto x=root=Node::create();
-//     root->size=n;
-//     forh(i, 0, n-1){
-//       auto cur=Node::create();
-//       cur->size=x->size-1-i;
-//       cur->p=x;
-//       x->r=cur;
-//       x=cur;
-//     }
-//     auto dummy = Node::null;
-//     dummy->r=root;
-//     dummy->l=x;
-//   }
-
-//   Node* nth(i64 ord){
-//     return root->nth(ord);
-//   }
-
-//   void upd(i64 i, i64 val){
-//     auto x = nth(i);
-//     x->splay(root);
-//     x->val+=val;
-//     x->acc+=val;
-//   }
-
-//   i64 query(i64 l, i64 r){
-//     auto x = nth(l-1);
-//     x->splay(root);
-//     auto y = nth(r+1);
-//     y->splay(root);
-//     return root->l->r->acc;
-//   }
-// };
-
-// SplayTree::Node* SplayTree::Node::null = nullptr;
