a2ab02641990548a03e28ab00503c94634a4beb6
LoboPrix <lobo_prix@naver.com>
2019-01-29 01:02:33 +0900

    Splay Lazy

--- Includes/SplayTree.h
+++ Includes/SplayTree.h
@@ -4,24 +4,27 @@
 struct SplayTree{
   SplayTree(int n, i64 id_upd, i64 id_qry,
     const function<i64(i64, i64)>& queryf,
-	  const function<i64(int, int, i64, i64)>& lazyf,
+	  const function<i64(int, i64, i64)>& lazyf,
 	  const function<i64(i64, i64)>& propaf)
     :n(n), id_upd(id_upd), id_qry(id_qry), queryf(queryf), lazyf(lazyf), propaf(propaf)
-  { Build(); }
+  { build(); }
 
 	SplayTree(
 		int n=0,
 		i64 id_upd=I64::zero(),
 		i64 id_qry=I64::zero())
 		:SplayTree(n, id_upd, id_qry,[](i64 a, i64 b) {return a + b; },
-    [id_upd, id_qry](int s, int e, i64 tval, i64 lval) {return tval + (lval != id_upd ? lval : id_upd) * (e - s); }, 
-    [id_upd, id_qry](i64 lval, i64 val) { return (lval != id_upd ? lval : id_upd) + val; })
+    [id_upd, id_qry](int cnt, i64 tval, i64 lval) {return tval + lval * cnt; }, 
+    [id_upd, id_qry](i64 lval, i64 val) { return lval + val; })
   {}
 
-  void update(int i, i64 z) {
-    splay(nth(root, i));
-    root->acc += z;
-    root->val += z;
+  void update(int i, i64 val) {
+    update(i, i+1, val);
+  }
+
+  void update(int s, int e, i64 val){
+    auto x=interval(s, e);
+    x->lazy=propaf(x->lazy, val);
   }
   
   i64 query(int s, int e) {
@@ -33,6 +36,7 @@ private:
     Node *p=nullptr, *l=nullptr, *r=nullptr;
     int sz=0;
     i64 val=0, acc=0;
+    i64 lazy=0;
 
     void adoptL(Node* n){l=n; if(n)n->p=this;}
     void adoptR(Node* n){r=n; if(n)n->p=this;}
@@ -42,13 +46,13 @@ private:
   int n;
   const int id_upd, id_qry;
 	const function<i64(i64, i64)> queryf;
-	const function<i64(int, int, i64, i64)> lazyf;
+	const function<i64(int, i64, i64)> lazyf;
 	const function<i64(i64, i64)> propaf;
 
   inline int size(Node* x)const{return x?x->sz:0;}
   inline i64 acc(Node* x)const{return x?x->acc:id_upd;}
 
-  void Build(){
+  void build(){
     Node* x = new Node();
     root=x;
     //+2 is Left(-1) Right(n) mock nodes
@@ -61,15 +65,23 @@ private:
   }
 
   //children should be renewed.
-  void renew(Node* x){
+  void renew(Node* x, bool parent = false){
     if(!x)
       return;
     x->sz=1+size(x->l)+size(x->r);
     x->acc=x->val+acc(x->l)+acc(x->r);
+
+    if(parent)
+      renew(x->p, parent);
   }
 
   Node* nth(Node* x, int n){
     assert(x);
+    update_lazy(x);
+    update_lazy(x->l);
+    update_lazy(x->r);
+    renew(x);
+
     int lsz=size(x->l);
     if(lsz>n+1)
       return nth(x->l, n);
@@ -126,5 +138,21 @@ private:
     root->p=sav;
     root=sav;
     return root->r->l;
+  }
+  
+  //should call before when accesses children. (example: nth, search, insert etc...)
+  //and access should be started at root.
+  void update_lazy(Node* x){
+    if(!x)
+      return;
+    x->val=lazyf(1, x->val, x->lazy);
+    x->acc=lazyf(x->sz, x->acc, x->lazy);
+    if(x->l){
+      x->l->lazy=propaf(x->l->lazy, x->lazy);
+    }
+    if(x->r){
+      x->r->lazy=propaf(x->r->lazy, x->lazy);
+    }
+    x->lazy=id_upd;
   }
 };
