a3d3bd59c85c8fda7caea7ff89224d981dfccce0
0xC0DEF <lobo_prix@naver.com>
2019-02-04 00:03:04 +0900

    CRLF 2 LF

--- Includes/RootedTree.h
+++ Includes/RootedTree.h
@@ -7,13 +7,16 @@ struct RootedTree:public Tree<T>{
   using Tree<T>::n;
   using Tree<T>::g;
   int root;
-  vector<int> parent;  
+  vector<int> parent;
+  decltype(g)& children;
 
-  RootedTree(int n, int r):Tree<T>(n),root(r),parent(n){dfs_parent(root, -1);}
+  RootedTree(int n, int r):Tree<T>(n),root(r),parent(n), children(Tree<T>::g){dfs_parent(root, -1);}
 protected:
   void dfs_parent(int cur, int p)const{
     parent[cur]=p;
-    for(auto &i:g[cur]){
+    if(p!=-1)
+      children[cur].erase(find_if(children[cur].begin(), children[cur].end(), [p](auto& a){return a.e==p;}));
+    for(auto &i:children[cur]){
       dfs_parent(i.e, cur);
     }
   }
@@ -24,6 +27,6 @@ protected:
   }
 
   int lca(int a, int b){
-
+    return 0;
   }
 };
