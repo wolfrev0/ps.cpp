d1f2014f83c996dcb36e65bb3eb90cc57178aef9
Lobo Prix <lobo_prix@naver.com>
2019-02-19 00:48:58 +0900

    SegTree Refactoring

--- Includes/SegTree.h
+++ Includes/SegTree.h
@@ -4,61 +4,43 @@
 template<typename T, typename U>
 struct SegFDefault{
 	static T idT(){return T();}
-	static T idU(){return U();}
 	static T q(const T& a, const T& b){return a+b;}
-	static T upd(const T& a, const U& b, int c){return a+b*c;}
-	static U propa(const U& a, const U& b){return a+b;}
+	static T upd(const T& a, const U& b){return a+b;}
 };
 
 //upperbound of 2^(ceil(log2(n))+1)/n is 4. (plot floor(2^(ceil(log2(x))+1)/x) from x=0 to 100000000)
 template<typename T, typename U=T, typename F=SegFDefault<T, U>>
 struct SegTree{
 	SegTree(int n)
-	:n(n),tree(4*n,F::idT()),lazy(4*n,F::idU())
+	:n(n),tree(4*n,F::idT())
 	{}
 
-	void update(int p, U val){update_range(1,0,n,p,p+1,val);}
-	void update(int s, int e, U val){update_range(1,0,n,s,e,val);}
-	T query(int s, int e, bool revord=false){return query2(1,0,n,s,e,revord);}
+	void update(int p, U val){update(1,0,n,p,val);}
+	T query(int s, int e){return query(1,0,n,s,e);}
 private:
 	const int n;
 	vector<T> tree;
-	vector<U> lazy;
 
-	void propagate(int cur, int cs, int ce){
-		if(lazy[cur]==F::idU())
+	void update(int cur, int cs, int ce, int p, U val){
+		if (p>=ce||p+1<=cs)
 			return;
-		tree[cur]=F::upd(tree[cur],lazy[cur],ce-cs);
-		if (ce-cs>1){
-			lazy[cur*2]=F::propa(lazy[cur*2],lazy[cur]);
-			lazy[cur*2+1]=F::propa(lazy[cur*2+1],lazy[cur]);
-		}
-		lazy[cur]=F::idU();
-	}
-
-	void update_range(int cur, int cs, int ce, int s, int e, U val){
-		propagate(cur,cs,ce);
-		if (s>=ce||e<=cs)
-			return;
-		if (s<=cs&&ce<=e){
-			lazy[cur]=F::propa(lazy[cur],val);
-      propagate(cur,cs,ce);
-			return;
+		if (p<=cs&&ce<=p+1){
+			tree[cur]=F::upd(tree[cur],val);
+      return;
 		}
     int m=(cs+ce)/2;
-		update_range(cur*2,cs,m,s,e,val);
-		update_range(cur*2+1,m,ce,s,e,val);
-		tree[cur]=F::q(tree[cur*2],tree[cur*2+1]);
+    update(cur*2,cs,m,p,val);
+    update(cur*2+1,m,ce,p,val);
+    tree[cur]=F::q(tree[cur*2],tree[cur*2+1]);
 	}
 
-	T query2(int cur, int cs, int ce, int s, int e, bool revord){
-		propagate(cur,cs,ce);
+	T query(int cur, int cs, int ce, int s, int e){
 		if (s>=ce||e<=cs)
 			return F::idT();
-		if (s<=cs&&ce<=e)
+		if (s<=cs&&ce<=e){
 			return tree[cur];
-		if(revord)
-			return F::q(query2(cur*2+1,(cs+ce)/2,ce,s,e,revord),query2(cur*2,cs,(cs+ce)/2,s,e,revord));
-		return F::q(query2(cur*2,cs,(cs+ce)/2,s,e,revord),query2(cur*2+1,(cs+ce)/2,ce,s,e,revord));
+		}
+    int m=(cs+ce)/2;
+		return F::q(query(cur*2,cs,m,s,e),query(cur*2+1,m,ce,s,e));
 	}
 };
---
+++ Includes/SegTreeCommutative.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Core.h"
+
+template<typename T, typename U>
+struct SegFDefault{
+	static T idT(){return T();}
+	static T q(const T& a, const T& b){return a+b;}
+	static T upd(const T& a, const U& b){return a+b;}
+};
+
+template<typename T, typename U, typename F=SegFDefault<T,U>>
+struct SegTreeCommutative{
+  SegTreeCommutative(int n):n(n),tree(4*n, F::idT()){}
+  int n;
+  vector<T> tree;
+
+  void update(int p, U val){
+    p+=n;
+    for (tree[p]=F::upd(tree[p], val); p>1; p>>=1)
+      tree[p>>1]=F::q(tree[p],tree[p^1]);
+  }
+
+  T query(int l, int r) {
+    T res=F::idT();
+    for (l+=n,r+=n; l<r; l>>=1,r>>=1) {
+      if (l&1)res=F::q(res,tree[l++]);
+      if (r&1)res=F::q(res,tree[--r]);
+    }
+    return res;
+  }
+};
