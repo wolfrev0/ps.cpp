8e22d576e631fce538ba3183fa721aaf486ac362
0xC0DEF <lobo_prix@naver.com>
2019-02-24 02:15:57 +0900

    Frac Bugfix & Update

--- Includes/Frac.h
+++ Includes/Frac.h
@@ -2,19 +2,21 @@
 #include "Core.h"
 
 template<typename T>
-struct Frac
-{
-	T a, b;
-	bool reduction;
-	Frac(T a=0, T b=1, bool reduction=false):a(a),b(b),reduction(reduction){normalize();}
-	u64 hash()const{return (u64)a*prime+(u64)b;}
-	bool operator<(const Frac& r)const { return (b<0) ^ (r.b<0) ? a * r.b > r.a*b : a*r.b < r.a*b; }
-	bool operator==(const Frac& r)const { return a * r.b == r.a*b; }
-	Frac operator*(const Frac& r)const { return {a*r.a, b*r.b};}
-	Frac operator/(const Frac& r)const { return *this * Frac(r.b, r.a);}
+struct Frac{
+	T a,b;
+	Frac(T a=0, T b=1):a(a),b(b){normalize();}
+	bool operator<(const Frac& r)const{return a*r.b<r.a*b;}
+	bool operator==(const Frac& r)const{return a*r.b==r.a*b;}
+	Frac operator*(const Frac& r)const{return {a*r.a, b*r.b};}
+	Frac operator/(const Frac& r)const{return (*this)*Frac(r.b,r.a);}
 	bool is_inf()const{return a&&!b;}
 	bool is_nan()const{return !a&&!b;}
+  bool is_int()const{return b&&!(a%b);}
+  //a is sign <if normalized>
+  int sign()const{return a>=0?1:-1;}
 	f64 to_f64(){return (f64)a/b;}
+  T ceil()const{return a/b+(sign()>0)*!!(a%b);}
+  T floor()const{return a/b-(sign()<0)*!!(a%b);}
 	void normalize(){
 		if(is_nan())
 			return;
@@ -22,18 +24,15 @@ struct Frac
 			a=1;
 			return;
 		}
-		if(reduction){
-			T g = gcd(a,b); 
-			a/=g; b/=g;
-			if(b<0)
-				a*=-1, b*=-1;
-		}
+
+    T g = gcd(a,b); 
+    a/=g; b/=g;
+    if(b<0)
+      a*=-1, b*=-1;
 	}
-	
-	static Frac zero(){ return 0; }
-	static Frac one(){ return 1; }
-	static Frac inf() { return {T::inf(),0}; }
+
+  static Frac<T> inf(){return {1,0};}
 };
 
 template<typename T>
-ostream& operator<<(ostream& s, const Frac<T>& n) { return s <<n.a<<'/'<<n.b; }
+ostream& operator<<(ostream& s, const Frac<T>& n){return s<<n.a<<'/'<<n.b;}
