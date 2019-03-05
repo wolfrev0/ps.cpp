79cd1a78a34902bc8fb1e95f51c0176a77784191
0xC0DEF <lobo_prix@naver.com>
2019-03-05 17:26:07 +0900

    .

---
+++ Includes/SegTree2D.h
@@ -0,0 +1,49 @@
+//not my code
+//I got this code from BOJ slack, but I forgot who wrote it.
+
+#pragma once
+#include "Core.h"
+
+const int L = (1<<10);
+​
+struct seg1d {
+	int v[2*L];
+	void upd (int P, int V) {
+		P += L;
+		while(P) {
+			v[P] += V;
+			P /= 2;
+		}
+	}
+	int get (int S, int E) {
+		int R = 0;
+		S += L; E += L;
+		while(S <= E) {
+			if(S % 2 == 1) R += v[S++];
+			if(E % 2 == 0) R += v[E--];
+			S /= 2; E /= 2;
+		}
+		return R;
+	}
+};
+​
+struct seg2d {
+	seg1d v[2*L];
+	void upd (int X, int Y, int V) {
+		X += L;
+		while(X) {
+			v[X].upd(Y, V);
+			X /= 2;
+		}
+	}
+	int get (int XS, int XE, int YS, int YE) {
+		int R = 0;
+		XS += L; XE += L;
+		while(XS <= XE) {
+			if(XS % 2 == 1) R += v[XS++].get(YS, YE);
+			if(XE % 2 == 0) R += v[XE--].get(YS, YE);
+			XS /= 2; XE /= 2;
+		}
+		return R;
+	}
+};
