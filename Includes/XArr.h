2ca79f7d32a31c0316d05e786e6c3c0f277d7a20
0xC0DEF <lobo_prix@naver.com>
2019-03-07 23:17:07 +0900

    Update XArr.h

--- Includes/XArr.h
+++ Includes/XArr.h
@@ -5,6 +5,8 @@ template<typename T>
 struct XArr{
   XArr(int n=0, int offset=1):offset(offset), arr(n+offset){}
   T& operator[](int idx){return arr[idx+offset];}
+  auto begin(){return arr.begin();}
+  auto end(){return arr.end();}
   int offset;
   vector<T> arr;
 };
