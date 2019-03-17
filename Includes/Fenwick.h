90dfc48028064f4d074bff24019aa1bf5abbf696
0xC0DEF <lobo_prix@naver.com>
2019-03-18 00:22:14 +0900

    Update Fenwick.h

--- Includes/Fenwick.h
+++ Includes/Fenwick.h
@@ -3,30 +3,31 @@
 
 template<typename T>
 struct FenwickTree{
-	int n;
-	FenwickTree(int n):n(n+1), tree(n+2, T::zero()){}
-	vector<T> tree;
+  int n;
+  FenwickTree(int n):n(n+1), tree(n+2, T()){}
+  vector<T> tree;
 
-	T query(int s, int e){
-		s++, e++;//ft idx base = 1
-		return sum(e-1)-sum(s-1);
-	}
+  T query(int i){return query(i, i+1);}
+  T query(int s, int e){
+    s++, e++;//ft idx base = 1
+    return sum(e-1)-sum(s-1);
+  }
 
-	void update(int i, T delta){
-		i++;
-		while(i<n){
-			tree[i]+=delta;
-			i+=(i&-i);
-		}
-	}
+  void update(int i, T delta){
+    i++;
+    while(i<n){
+      tree[i]+=delta;
+      i+=(i&-i);
+    }
+  }
 
 private:
-	T sum(int i){
-		T ans=T::zero();
-		while(i>0){
-			ans+=tree[i];
-			i-=(i&-i);
-		}
-		return ans;
-	}
+  T sum(int i){
+    T ans=T();
+    while(i>0){
+      ans+=tree[i];
+      i-=(i&-i);
+    }
+    return ans;
+  }
 };
