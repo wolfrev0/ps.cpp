a2c25192419b055c3519c51168be34479fab1a5a
0xC0DEF <lobo_prix@naver.com>
2019-03-28 12:59:29 +0900

    forh safety update

--- Includes/Core.h
+++ Includes/Core.h
@@ -14,14 +14,24 @@ const f64 eps=1e-12;
 const i32 prime=i32(2e9 + 11);
 i32 mod=i32(1e9 + 7);
 template<typename T> T inf() { return numeric_limits<T>::max() / 2; }
-#define forh(var, s, e) for(int var=s; (int)s<=var && var<(int)e; ++var)//for: half-opened range
-#define forhi(var, s, e) for(int var=e-1; (int)s<=var && var<(int)e; --var)//inversion
-#define forho(var, s, e) int var=s; for(; (int)s<=var && var<(int)e; ++var)//out declaration
-#define forhoi(var, s, e) int var=e-1; for(; (int)s<=var && var<(int)e; --var)
-#define forc(var, s, e) for(int var=s; (int)s<=var && var<=(int)e; ++var)//for: closed range
-#define forci(var, s, e) for(int var=e; (int)s<=var && var<=(int)e; --var)//inversion
-#define forco(var, s, e) int var=s; for(; (int)s<=var && var<=(int)e; ++var)//out declaration
-#define forcoi(var, s, e) int var=e; for(; (int)s<=var && var<=(int)e; --var)
+#define forh(var, s, e) for(int var=s; int(s)<=var && var<int(e); ++var)//for: half-opened range
+#define forhi(var, s, e) for(int var=e-1; int(s)<=var && var<int(e); --var)//inversion
+#define forho(var, s, e) int var=s; for(; int(s)<=var && var<int(e); ++var)//out declaration
+#define forhoi(var, s, e) int var=e-1; for(; int(s)<=var && var<int(e); --var)
+#define forc(var, s, e) for(int var=s; int(s)<=var && var<=int(e); ++var)//for: closed range
+#define forci(var, s, e) for(int var=e; int(s)<=var && var<=int(e); --var)//inversion
+#define forco(var, s, e) int var=s; for(; int(s)<=var && var<=int(e); ++var)//out declaration
+#define forcoi(var, s, e) int var=e; for(; int(s)<=var && var<=int(e); --var)
+
+string itos(int i){
+  if(!i)
+    return "0";
+  string ret;
+  while(i)
+    ret.push_back(i%10+'0'), i/=10;
+  reverse(ret.begin(), ret.end());
+  return ret;
+}
 
 //Extensions
 #include <ext/pb_ds/assoc_container.hpp>
