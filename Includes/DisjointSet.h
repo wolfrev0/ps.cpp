2b5f2ceb44bab7d130c918fbc38fe13dd4fbad11
0xC0DEF <lobo_prix@naver.com>
2019-04-01 15:44:23 +0900

    Update DisjointSet.h
    Added Components Size

--- Includes/DisjointSet.h
+++ Includes/DisjointSet.h
@@ -3,24 +3,26 @@
 
 struct DisjointSet {
   vector<int> par;
+  vector<int> sz;
 
-  DisjointSet(int n) :par(n) {
-    forh(i, 0, n)
-      par[i] = i;
+  DisjointSet(int n): par(n),sz(n,1){
+    forh(i,0,n)
+      par[i]=i;
   }
 
-  void uni(int a, int b) {
-    a = find(a);
-    b = find(b);
-    //if (a == b)
-    //return;
-    //sometimes this if statement needed. (ex: BOJ-4195)
-    par[b] = a;
+  void uni(int a, int b){
+    a=find(a), b=find(b);
+    if(a==b)
+      return;
+    par[b]=a;
+    sz[a]+=sz[b];
   }
 
-  int find(int a) {
-    if (par[a] == a)
+  int find(int a){
+    if(par[a]==a)
       return a;
-    return par[a] = find(par[a]);
+    return par[a]=find(par[a]);
   }
+
+  int size(int a){return sz[find(a)];}
 };
