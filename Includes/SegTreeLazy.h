4a38e8b567d3ee0ae9a4a7f278eb55895acdf525
0xC0DEF <lobo_prix@naver.com>
2019-04-21 03:01:55 +0900

    Update SegTreeLazy.h

--- Includes/SegTreeLazy.h
+++ Includes/SegTreeLazy.h
@@ -4,7 +4,7 @@
 template<typename T, typename U>
 struct LazyFSumAdd{
 	static T idT(){return T();}
-	static T idU(){return U();}
+	static U idU(){return U();}
 	static T q(const T& a, const T& b){return a+b;}
 	static T upd(const T& a, const U& b, int c){return a+b*c;}
 	static U propa(const U& a, const U& b){return a+b;}
@@ -13,7 +13,7 @@ struct LazyFSumAdd{
 template<typename T, typename U>
 struct LazyFMaxAdd{
 	static T idT(){return T();}
-	static T idU(){return U();}
+	static U idU(){return U();}
 	static T q(const T& a, const T& b){return max(a,b);}
 	static T upd(const T& a, const U& b, int c){return a+b;}
 	static U propa(const U& a, const U& b){return a+b;}
@@ -22,13 +22,22 @@ struct LazyFMaxAdd{
 //upperbound of 2^(ceil(log2(n))+1)/n is 4. (plot floor(2^(ceil(log2(x))+1)/x) from x=0 to 100000000)
 template<typename T, typename U=T, typename F=LazyFSumAdd<T, U>>
 struct SegTreeLazy{
-	SegTreeLazy(int n)
-	:n(n),tree(4*n,F::idT()),lazy(4*n,F::idU())
+	SegTreeLazy(int n, T init=F::idT())
+	:n(n),tree(4*n, init),lazy(4*n,F::idU())
 	{}
 
 	void update(int p, U val){update(1,0,n,p,p+1,val);}
 	void update(int s, int e, U val){update(1,0,n,s,e,val);}
-	T query(int s, int e, bool revord=false){return query(1,0,n,s,e,revord);}
+	T query(int s, int e){return query(1,0,n,s,e);}
+
+	T tmp(int cur, int cs, int ce, int s, int e){
+		propagate(cur,cs,ce);
+		if (s>=ce||e<=cs)
+			return F::idT();
+		if (s<=cs&&ce<=e)
+			return tree[cur]==-1?inf():tree[cur];
+		return F::q(tmp(cur*2,cs,(cs+ce)/2,s,e)==-1?inf():tmp(cur*2,cs,(cs+ce)/2,s,e),tmp(cur*2+1,(cs+ce)/2,ce,s,e)==-1?inf():tmp(cur*2+1,(cs+ce)/2,ce,s,e));
+	}
 private:
 	const int n;
 	vector<T> tree;
@@ -60,14 +69,12 @@ private:
 		tree[cur]=F::q(tree[cur*2],tree[cur*2+1]);
 	}
 
-	T query(int cur, int cs, int ce, int s, int e, bool revord){
+	T query(int cur, int cs, int ce, int s, int e){
 		propagate(cur,cs,ce);
 		if (s>=ce||e<=cs)
 			return F::idT();
 		if (s<=cs&&ce<=e)
 			return tree[cur];
-		if(revord)
-			return F::q(query(cur*2+1,(cs+ce)/2,ce,s,e,revord),query(cur*2,cs,(cs+ce)/2,s,e,revord));
-		return F::q(query(cur*2,cs,(cs+ce)/2,s,e,revord),query(cur*2+1,(cs+ce)/2,ce,s,e,revord));
+		return F::q(query(cur*2,cs,(cs+ce)/2,s,e),query(cur*2+1,(cs+ce)/2,ce,s,e));
 	}
 };
