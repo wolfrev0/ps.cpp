c6c30b44f2d31309e53d8c42005d1c6424f138ed
0xC0DEF <lobo_prix@naver.com>
2019-05-04 00:36:15 +0900

    Create Template.cpp

---
+++ Examples/Template.cpp
@@ -0,0 +1,12 @@
+#include "Core.h"
+//Do not use it when solving interactive problems!!!
+#define endl '\n'
+//#define int i64
+
+signed main(){
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	cout<<fixed<<setprecision(11);
+	srand((u32)time(0));
+	
+	return 0;
+}
