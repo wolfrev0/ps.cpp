0dafb5f6383ca08398c0555a8aea769973ecfce4
0xC0DEF <lobo_prix@naver.com>
2019-05-07 09:37:52 +0900

    Dynamic CHT

--- Includes/Core.h
+++ Includes/Core.h
@@ -3,19 +3,18 @@
   #pragma GCC optimize ("Ofast")
 #endif
 #include <bits/stdc++.h>
+using namespace std;
+
+//Extensions: gp_hash_table, XSet
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+//Extensions: rope
+#include <ext/rope>
+using namespace __gnu_cxx;
 
 //Syntax Sugars
-using namespace std;
-using f64 = double;
-using i64=long long;
-using u64=unsigned long long;
-
-const f64 pi=acos(-1);
-const f64 eps=1e-12;
-const int prime=998244353;//1073741783 1073741789 int(2e9+11) int(1e9+9)
-int mod=int(1e9+7);
-template<typename T> T inf() { return numeric_limits<T>::max() / 2; }
-
 #define forh(var, s, e) for(int var=s; int(s)<=var && var<int(e); ++var)//for: half-opened range
 #define forhi(var, s, e) for(int var=e-1; int(s)<=var && var<int(e); --var)//inversion
 #define forho(var, s, e) int var=s; for(; int(s)<=var && var<int(e); ++var)//out declaration
@@ -27,12 +26,15 @@ template<typename T> T inf() { return numeric_limits<T>::max() / 2; }
 #define rep(x) forh(repi,0,x)
 #define all(x) x.begin(),x.end()
 #define sum_in(s,e) accumulate(s,e,0ll)
+//NOTE: *mri(it) == *prev(it)
+#define mri(it) make_reverse_iterator(it)
 
-//Extensions: gp_hash_table, XSet
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
-//Extensions: rope
-#include <ext/rope>
-using namespace __gnu_cxx;
+using f64 = double;
+using i64=long long;
+using u64=unsigned long long;
+const f64 pi=acos(-1);
+const f64 eps=1e-12;
+const int prime=998244353;//1073741783 1073741789 int(2e9+11) int(1e9+9)
+int mod=int(1e9+7);
+template<typename T> T inf() { return numeric_limits<T>::max() / 2; }
+template<typename C, typename RI> RI rerase(C& c, RI ri){return next(mri(c.erase(prev(ri.base()))));}
--- Includes/DPOpt.h
+++ Includes/DPOpt.h
@@ -27,21 +27,26 @@ vector<vector<i64>> knuth_opt(const vector<i64>& init, const function<i64(int,in
 //min, max, j<i, i<j<n 모두 가능하다. 그림을 잘 생각해서 써라.
 //정밀도가 문제가 된다면 x좌표 말고 ccw로 처리하는 방법도 있다. 아래링크 참고
 //https://cp-algorithms.com/geometry/convex_hull_trick.html
-//BOJ_17168의 koosaga, kdh9949 코드도 ccw 방법이다.
+struct Line{
+	i64 tan;
+	mutable i64 yic;
+	mutable f64 lx=-1/0.0, rx=1/0.0;
+
+	bool operator<(const Line& r)const{return tan<r.tan;}
+	bool operator<(const i64 x)const{return rx<x;}
+
+	f64 intersectX(const Line& r)const{return (r.yic-yic)/f64(tan-r.tan);} 
+	i64 f(i64 x)const{return tan*x+yic;}
+};
+
 struct CHTStack{
-	struct Line{
-		i64 tan, yic;
-		f64 px;
-		Line(i64 tan, i64 yic, f64 px):tan(tan), yic(yic), px(px){}
-		f64 intersectX(const Line& r)const{return (r.yic-yic)/f64(tan-r.tan);}
-	};
 	vector<Line> st;
 	
 	void push(i64 tan, i64 yic){
-		Line f(tan, yic, 0);
+		Line f{tan, yic, 0};
 		while(st.size()){
-			f.px=st.back().intersectX(f);
-			if(tan==st.back().tan || f.px<st.back().px)
+			f.lx=st.back().intersectX(f);
+			if(tan==st.back().tan || f.lx<st.back().lx)
 				st.pop_back();
 			else
 				break;
@@ -53,8 +58,37 @@ struct CHTStack{
 		int s=0, e=st.size();
 		while(e-s>1){
 			int m=(s+e)/2;
-			(x<st[m].px?e:s)=m;
+			(x<st[m].lx?e:s)=m;
 		}
 		return st[s].tan*x + st[s].yic;
 	}
+};
+
+struct DynCHT{
+	void insert(i64 a, i64 b){
+		auto it=s.find({a, b});
+		if(it!=s.end())
+			b=max(b, it->yic), s.erase(it);
+		Line f={a,b};
+
+		auto r=s.upper_bound({a,b});
+		while(r!=s.end() && f.intersectX(*r)>r->rx)
+			r=s.erase(r);
+		auto l=mri(s.lower_bound({a,b}));
+		while(l!=s.rend() && f.intersectX(*l)<l->lx)
+			l=rerase(s, l);
+		
+		if(r!=s.end())
+			f.rx=r->lx=f.intersectX(*r);
+		if(l!=s.rend())
+			f.lx=l->rx=f.intersectX(*l);
+		s.insert(f);
+	}
+
+	i64 query(i64 x){
+		auto it=s.lower_bound(x);
+		return it->tan*x+it->yic;
+	}
+private:
+	set<Line, less<>> s;
 };
