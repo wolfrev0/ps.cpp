9bd0b3ecef9c7bbe44a9d8da03fc750dea256032
Lobo <lobo_prix@naver.com>
2019-06-23 07:29:46 +0000

    vector가 아닌 array로 바꾼 이유는 다음과 같다. - 크기가 항상 고정되있다. - 실행전에 최대 크기를 알 수 있으며, 최대크기 미만의 n이 들어오더라도 최대크기로 사용해도 된다(어짜피 세그트리 계산에 항등원 T()가 필요하기 때문에, 남는 공간은 항등원T()으로 채우면 된다.)

--- Includes/SegLazy.h
+++ Includes/SegLazy.h
@@ -1,118 +1,78 @@
 #pragma once
 #include "Core.h"
-#include "GccExts.h"
 
-template<typename T, typename U>
-struct LazyFSumAdd{
-	static T q(const T& a, const T& b){return a+b;}
-	static T upd(const T& a, const U& b, int c){return a+b*c;}
-	static U propa(const U& a, const U& b){return a+b;}
-};
-
-template<typename T, typename U>
-struct LazyFMaxAdd{
-	static T q(const T& a, const T& b){return max(a,b);}
-	static T upd(const T& a, const U& b, int c){return a+b;}
-	static U propa(const U& a, const U& b){return a+b;}
-};
-
-//upperbound of 2^(ceil(log2(n))+1)/n is 4. (plot floor(2^(ceil(log2(x))+1)/x) from x=0 to 100000000)
-template<typename T, typename U=T, typename F=LazyFSumAdd<T, U>>
+template<int n, typename T, typename U=T>
 struct SegLazy{
-	SegLazy(int n)
-	:n(n),tree(4*n),lazy(4*n)
-	{}
+	T q(int p){return q(p,p+1);}
+	T q(int s, int e){return q(1,0,n,s,e);}
+	void upd(int p, U val){upd(p, p+1, val);}
+	void upd(int s, int e, U val){upd(1,0,n,s,e,val);}
+protected:
+	virtual T qf(const T& a, const T& b)=0;
+	virtual T uf(const T& a, const U& b, int c)=0;
+	virtual U propaf(const U& a, const U& b)=0;
 
-	void update(int p, U val){update(1,0,n,p,p+1,val);}
-	void update(int s, int e, U val){update(1,0,n,s,e,val);}
-	T query(int s, int e){return query(1,0,n,s,e);}
-private:
-	const int n;
-	vector<T> tree;
-	vector<U> lazy;
-
-	void propagate(int cur, int cs, int ce){
-		if(lazy[cur]==U())
-			return;
-		tree[cur]=F::upd(tree[cur],lazy[cur],ce-cs);
-		if (ce-cs>1){
-			lazy[cur*2]=F::propa(lazy[cur*2],lazy[cur]);
-			lazy[cur*2+1]=F::propa(lazy[cur*2+1],lazy[cur]);
-		}
-		lazy[cur]=U();
-	}
-
-	void update(int cur, int cs, int ce, int s, int e, U val){
-		propagate(cur,cs,ce);
-		if (s>=ce||e<=cs)
-			return;
-		if (s<=cs&&ce<=e){
-			lazy[cur]=F::propa(lazy[cur],val);
-			propagate(cur,cs,ce);
-			return;
-		}
-		int m=(cs+ce)/2;
-		update(cur*2,cs,m,s,e,val);
-		update(cur*2+1,m,ce,s,e,val);
-		tree[cur]=F::q(tree[cur*2],tree[cur*2+1]);
-	}
-
-	T query(int cur, int cs, int ce, int s, int e){
-		propagate(cur,cs,ce);
+	T q(int cur, int cs, int ce, int s, int e){
+		propa(cur, cs, ce);
 		if (s>=ce||e<=cs)
 			return T();
 		if (s<=cs&&ce<=e)
 			return tree[cur];
-		return F::q(query(cur*2,cs,(cs+ce)/2,s,e),query(cur*2+1,(cs+ce)/2,ce,s,e));
+		int m=(cs+ce)/2;
+		return qf(q(cur*2,cs,m,s,e),q(cur*2+1,m,ce,s,e));
 	}
+
+	void upd(int cur, int cs, int ce, int s, int e, U val){
+		propa(cur, cs, ce);
+		if (s>=ce||e<=cs)
+			return;
+		if (s<=cs&&ce<=e){
+			holdlazy(cur, val);
+			propa(cur, cs, ce);
+			return;
+		}
+		int m=(cs+ce)/2;
+		upd(cur*2,cs,m,s,e,val);
+		upd(cur*2+1,m,ce,s,e,val);
+		tree[cur]=qf(tree[cur*2],tree[cur*2+1]);
+	}
+	
+	void propa(int cur, int cs, int ce){
+		if(!dirty[cur])
+			return;
+		tree[cur]=uf(tree[cur],lazy[cur],ce-cs);
+		dirty[cur]=false;
+		if(ce-cs>1){
+			holdlazy(cur*2,lazy[cur]);
+			holdlazy(cur*2+1,lazy[cur]);
+		}
+	}
+	
+	void holdlazy(int v, U val){
+		lazy[v]=dirty[v]?propaf(lazy[v],val):val;
+		dirty[v]=true;
+	}
+	
+	array<T,4*n> tree;
+	array<T,4*n> lazy;
+	array<bool,4*n> dirty;
 };
 
-template<typename T, typename U=T, typename F=LazyFSumAdd<T, U>>
-struct DynSegLazy{
-	DynSegLazy(int n)
-	:n(n)//,tree(4*n),lazy(4*n)
-	{}
-
-	void update(int p, U val){update(1,0,n,p,p+1,val);}
-	void update(int s, int e, U val){update(1,0,n,s,e,val);}
-	T query(int s, int e){return query(1,0,n,s,e);}
-private:
-	const int n;
-	cc_hash_table<int,T> tree;
-	cc_hash_table<int,U> lazy;
-
-	void propagate(int cur, int cs, int ce){
-		if(lazy[cur]==U())
-			return;
-		tree[cur]=F::upd(tree[cur],lazy[cur],ce-cs);
-		if (ce-cs>1){
-			lazy[cur*2]=F::propa(lazy[cur*2],lazy[cur]);
-			lazy[cur*2+1]=F::propa(lazy[cur*2+1],lazy[cur]);
-		}
-		lazy[cur]=U();
-	}
-
-	void update(int cur, int cs, int ce, int s, int e, U val){
-		propagate(cur,cs,ce);
-		if (s>=ce||e<=cs)
-			return;
-		if (s<=cs&&ce<=e){
-			lazy[cur]=F::propa(lazy[cur],val);
-			propagate(cur,cs,ce);
-			return;
-		}
-		int m=(cs+ce)/2;
-		update(cur*2,cs,m,s,e,val);
-		update(cur*2+1,m,ce,s,e,val);
-		tree[cur]=F::q(tree[cur*2],tree[cur*2+1]);
-	}
-
-	T query(int cur, int cs, int ce, int s, int e){
-		propagate(cur,cs,ce);
-		if (s>=ce||e<=cs)
-			return T();
-		if (s<=cs&&ce<=e)
-			return tree[cur];
-		return F::q(query(cur*2,cs,(cs+ce)/2,s,e),query(cur*2+1,(cs+ce)/2,ce,s,e));
-	}
+template<int n, typename T, typename U=T>
+struct SegLazySumAdd:public SegLazy<n,T,U>{
+	T qf(const T& a, const T& b)override{return a+b;}
+	T uf(const T& a, const U& b, signed c)override{return a+b*c;}
+	U propaf(const U& a, const U& b)override{return a+b;}
+};
+template<int n, typename T, typename U=T>
+struct SegLazyMaxAdd:public SegLazy<n,T,U>{
+	T qf(const T& a, const T& b)override{return max(a,b);}
+	T uf(const T& a, const U& b, signed c)override{return a+b;}
+	U propaf(const U& a, const U& b)override{return a+b;}
+};
+template<int n, typename T, typename U=T>
+struct SegLazySumAss:public SegLazy<n,T,U>{
+	T qf(const T& a, const T& b)override{return a+b;}
+	T uf(const T& a, const U& b, signed c)override{return b*c;}
+	U propaf(const U& a, const U& b)override{return b;}
 };
