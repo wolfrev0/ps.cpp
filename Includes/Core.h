78aed7e9d2d6234d09928f28c6af01dc17e0c127
Lobo <lobo_prix@naver.com>
2019-07-13 07:11:28 +0000

    Core Update(more safe&simplified by C++17)

--- Includes/Core.h
+++ Includes/Core.h
@@ -17,20 +17,21 @@ using f64 = double;using i64=long long;using u64=unsigned long long;
 #define cforoi(var, s, e) hforoi(var, s, (e)+1)
 #define rep(x) hfor(repi,0,x)
 #define all(x) x.begin(),x.end()
-#define sum_in(s,e) accumulate(s,e,0ll)
-#define mri(it) make_reverse_iterator(it)//*mri(it) == *prev(it)
 #define pb push_back
 #define eb emplace_back
 #define fi first
 #define se second
-#define sz(x) (int)size(x)
 #define gcd __gcd
 
-template<typename C, typename RI> RI rerase(C& c, RI ri){return next(mri(c.erase(prev(ri.base()))));}
 template<typename T> constexpr T inf() { return numeric_limits<T>::max() / 2; }
-int rand(int lb, int ub){static mt19937 rng(time(0)^i64(new int)); return uniform_int_distribution<int>(lb, ub-1)(rng);}
-int rand(int ub){return rand(0,ub);}
-int rand(){return rand(inf<int>());}
+auto mri(auto it){ return make_reverse_iterator(it); }//*mri(it) == *prev(it)
+auto rerase(auto& c, auto ri){ return next(mri(c.erase(prev(ri.base())))); }
+auto accumulate(auto a, auto b, int c=0){ return std::accumulate(a,b,i64(c)); }
+auto sum(auto a, auto b){return accumulate(a,b);}
+int size(auto x){ return (int)x.size(); }
+int rd(int lb, int ub){static mt19937 rng(time(0)^i64(new int)); return uniform_int_distribution<int>(lb, ub-1)(rng);}
+int rd(int ub=inf<int>()){return rd(0,ub);}
+#define random rd
 
 const f64 pi=acos(-1), eps=1e-12;
 const int prime=998244353;//1073741783 int(2e9+11) int(1e9+9)
