331bc086582b935d7beca744eafbe44136c1a0a8
Lobo <lobo_prix@naver.com>
2019-07-14 03:46:24 +0000

    Change LCA Algorithm from SparseTable to EulerTour+Seg

--- Includes/RootedTree.h
+++ Includes/RootedTree.h
@@ -1,11 +1,27 @@
 #pragma once
 #include "Core.h"
+#include "SegBU.h"
 
+//Is Dynamic Rooted Tree Useful??
+//Disjoint Set, BBST, Link Cut Tree belongs to it.
+//But I think it doesn't need now.
+
+//Static Rooted Tree
 template<typename T>
 struct RootedTree{
 	struct Edge{ int e; T w; };
+	struct SegLCA:public SegBU<int, 200000>{
+		const RootedTree& p;
+		SegLCA(const RootedTree& p):SegBU<int,200000>(inf<int>()), p(p){}
+		int fq(const int& a, const int& b)override{
+			if(a==inf<int>())return b;
+			if(b==inf<int>())return a;
+			return p.d[a]<p.d[b]?a:b;
+		}
+	};
 	
-	RootedTree(const vector<int>& par, const vector<T>& pw):n(sz(par)), ch(n), d(n), anc(n){
+	RootedTree(const vector<int>& par, const vector<T>& pw)
+		:n(sz(par)), ch(n), d(n), rpos(n), st(*this){
 		hfor(i,0,n)
 			p.pb({par[i], pw[i]});
 		r=0;
@@ -18,49 +34,26 @@ struct RootedTree{
 	}
 	
 	int lca(int a, int b){
-		if(d[a]>d[b])
+		if(rpos[a]>rpos[b])
 			swap(a,b);
-		int diff=d[b]-d[a];
-		while(diff){
-			b=getanc(b, __builtin_ctz(diff));
-			diff -= diff&-diff;
-		}
-		if(a==b)
-			return a;
-		hfori(i,0,32)
-			if(getanc(a,i)!=getanc(b,i))
-				a=getanc(a,i), b=getanc(b,i);
-		return getanc(a,0);
+		return st.q(rpos[a], rpos[b]+1);
 	}
 	
-	//vector<int> pre_tour(){}
-	//vector<int> in_tour(){}
-	//vector<int> post_tour(){}
-	//vector<int> euler_tour(){}
 protected:
-	int n, r;
+	int n,r;
 	vector<Edge> p;
 	vector<vector<Edge>> ch;
-	//anc[i][j] = (2^j)th ancestor of i
 	vector<int> d;
-	vector<vector<int>> anc;
-	int getanc(int i, int j){
-		if(j<0)
-			return i;
-		if(sz(anc[i])<=j)
-			return -1;
-		return anc[i][j];
-	}
-
-	vector<int> pbuf;
+	vector<int> rpos;
+	vector<int> etour;
+	//vector<int> pre_tour, post_tour; cant define infix
+	SegLCA st;
 	void dfs_init(int cur){
 		d[cur] = p[cur].e>=0 ? d[p[cur].e]+1 : 0;
-		for(int i=1;i<=sz(pbuf);i*=2)
-			anc[cur].pb(*(pbuf.end()-i));
+		st.upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
 		for(const auto &i:ch[cur]){
-			pbuf.pb(cur);
 			dfs_init(i.e);
-			pbuf.eb();
+			st.upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
 		}
 	}
 };
