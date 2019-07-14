6c8c2e7fb78980b2e8b7c0227b04e5523d161bce
0xC0DEF <lobo_prix@naver.com>
2019-07-14 13:17:18 +0900

    Update SegmentIntersect&MaximumFlow(BOJ-14750).cpp

--- Examples/SegmentIntersect&MaximumFlow(BOJ-14750).cpp
+++ Examples/SegmentIntersect&MaximumFlow(BOJ-14750).cpp
@@ -1,20 +1,16 @@
-#include "Core.h"
 #include "Line.h"
 #include "FlowGraph.h"
+#define endl '\n'//not interactive?
+//#define int i64//overflow?
 
-int main()
-{
-	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-	cout << fixed << setprecision(10);
-	srand((unsigned)time(0));
-
+signed main(){
 	int n, k, h, m;
 	cin >> n >> k >> h >> m;
 	FlowGraph g(m + h);
 	vector<Segment<f64>> lines;
 	Vec2<f64> pv;
 	cin >> pv.x >> pv.y;
-	forh(i, 1, n)
+	hfor(i, 1, n)
 	{
 		Vec2<f64> v;
 		cin >> v.x >> v.y;
@@ -22,17 +18,17 @@ int main()
 		pv = Vec2<f64>(v.x, v.y);
 	}
 	vector<Vec2<f64>> holes;
-	forh(i, 0, h)
+	hfor(i, 0, h)
 	{
 		int x, y;
 		cin >> x >> y;
 		holes.emplace_back(x, y);
 	}
-	forh(i, 0, m)
+	hfor(i, 0, m)
 	{
 		Vec2<f64> mp;
 		cin >> mp.x >> mp.y;
-		forh(j, 0, h)
+		hfor(j, 0, h)
 		{
 			bool fail = false;
 			for (auto k : lines)
@@ -48,7 +44,7 @@ int main()
 		}
 		g.add_edge(g.src, i, 1, 0);
 	}
-	forh(i, 0, h)
+	hfor(i, 0, h)
 		g.add_edge(m + i, g.snk, k, 0);
 
 	cout << (g.mf() == m ? "Possible" : "Impossible") << endl;
