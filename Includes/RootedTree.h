7e46d4b8903f81a4ced75fbb55d7573cc3a7d49e
Lobo <lobo_prix@naver.com>
2019-07-14 03:57:01 +0000

    RootedTree LCA Refactoring

--- Includes/RootedTree.h
+++ Includes/RootedTree.h
@@ -8,20 +8,11 @@
 
 //Static Rooted Tree
 template<typename T>
-struct RootedTree{
+struct RootedTree:private SegBU<int, 200000>{
 	struct Edge{ int e; T w; };
-	struct SegLCA:public SegBU<int, 200000>{
-		const RootedTree& p;
-		SegLCA(const RootedTree& p):SegBU<int,200000>(inf<int>()), p(p){}
-		int fq(const int& a, const int& b)override{
-			if(a==inf<int>())return b;
-			if(b==inf<int>())return a;
-			return p.d[a]<p.d[b]?a:b;
-		}
-	};
 	
 	RootedTree(const vector<int>& par, const vector<T>& pw)
-		:n(sz(par)), ch(n), d(n), rpos(n), st(*this){
+		:SegBU<int, 200000>(inf<int>()), n(sz(par)), ch(n), d(n), rpos(n){
 		hfor(i,0,n)
 			p.pb({par[i], pw[i]});
 		r=0;
@@ -36,7 +27,7 @@ struct RootedTree{
 	int lca(int a, int b){
 		if(rpos[a]>rpos[b])
 			swap(a,b);
-		return st.q(rpos[a], rpos[b]+1);
+		return q(rpos[a], rpos[b]+1);
 	}
 	
 protected:
@@ -47,13 +38,17 @@ protected:
 	vector<int> rpos;
 	vector<int> etour;
 	//vector<int> pre_tour, post_tour; cant define infix
-	SegLCA st;
+	int fq(const int& a, const int& b)override{
+		if(a==inf<int>())return b;
+		if(b==inf<int>())return a;
+		return d[a]<d[b]?a:b;
+	}
 	void dfs_init(int cur){
 		d[cur] = p[cur].e>=0 ? d[p[cur].e]+1 : 0;
-		st.upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
+		upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
 		for(const auto &i:ch[cur]){
 			dfs_init(i.e);
-			st.upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
+			upd(rpos[cur]=sz(etour), cur), etour.pb(cur);
 		}
 	}
 };
