354b34d6f7b79724542e17a525d9b91afbb1d982
0xC0DEF <lobo_prix@naver.com>
2019-12-05 21:46:56 +0900

    tpl.cpp indent correction

--- tpl.cpp
+++ tpl.cpp
@@ -3,7 +3,8 @@
 //#define int i64//overflow?
 using pii = pair<int,int>;
 ostream& operator<<(ostream& s, const pii& a) { return s<<'('<<a.fi<<','<<a.se<<')'; }
-//
+
 signed main(){
 	return 0;
-	}
+}
+
