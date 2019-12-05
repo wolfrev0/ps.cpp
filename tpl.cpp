7c733a4283055971c4a7bfa4e8d3b6c0c93b2108
0xC0DEF <lobo_prix@naver.com>
2019-12-06 05:40:55 +0900

    New Environment

---
+++ tpl.cpp
@@ -0,0 +1,9 @@
+#include "Core.h"
+#define endl '\n'//not interactive?
+//#define int i64//overflow?
+using pii = pair<int,int>;
+ostream& operator<<(ostream& s, const pii& a) { return s<<'('<<a.fi<<','<<a.se<<')'; }
+//
+signed main(){
+	return 0;
+	}
