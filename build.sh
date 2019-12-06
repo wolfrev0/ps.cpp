7fd7469b32a8840917f974b284a0bd9e58bee961
0xc0def <lobo_prix@naver.com>
2019-12-06 12:45:51 +0900

    build default value

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+src=${1:-"main.cpp"}
 if [ "$2" == "RELEASE" ]; then
-	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
+	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $src
 else
-	g++ -iquote ./includes -g -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
+	g++ -iquote ./includes -g -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $src
 fi
+./bin/merger.out $src ./includes
