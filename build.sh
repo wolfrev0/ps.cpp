3ac0e32b3e23f8c22e3a8f9180453525a5aa4604
Guest <guest-qmlufi@h29584.sgb.skir.kr>
2019-12-06 12:34:21 +0900

    Setting Automation

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$2" == "RELEASE" ]; then
-	mkdir -p ./bin && g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
+	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
 else
-	mkdir -p ./bin && g++ -iquote ./includes -g -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
+	g++ -iquote ./includes -g -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $1 && ./bin/merger.out $1 ./includes
 fi
