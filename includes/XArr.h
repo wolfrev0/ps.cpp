1609f3d7bce59f37dc82965c416ad84c09adcd91
0xc0def <lobo_prix@naver.com>
2020-01-18 23:17:00 +0900

    Rename Xarr to XArr

--- includes/XArr.h
+++ includes/XArr.h
@@ -2,8 +2,8 @@
 #include "Core.h"
 
 template<typename T>
-struct Xarr: public Arr<T>{
-	Xarr(int n=0, const T& init=T(), int offset=1):Arr<T>(n+offset, init), o(offset){}
+struct XArr: public Arr<T>{
+	XArr(int n=0, const T& init=T(), int offset=1):Arr<T>(n+offset, init), o(offset){}
 	T& operator[](int i){return at(i);}
 	T& at(int i){return Arr<T>::at(i+o);}
 	const T& operator[](int i)const{return at(i);}
