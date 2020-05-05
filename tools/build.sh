548c929510b0f63143ecaa8a5059fdaae9dd7b35
0xc0def <lobo_prix@naver.com>
2020-05-05 18:49:19 +0900

    Improve build debug option

--- tools/build.sh
+++ tools/build.sh
@@ -4,6 +4,6 @@ mkdir -p ./bin >/dev/null 2>&1
 if [ "$2" == "RELEASE" ]; then
 	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $src
 else
-	g++ -iquote ./includes -g -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $src
+	g++ -iquote ./includes -g3 -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $src
 fi
 ./bin/merger.out $src ./includes
