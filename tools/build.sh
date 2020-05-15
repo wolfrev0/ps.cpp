3c3cb02f9d54d2092fb465daa6b79f73f6c2c312
0xc0def <lobo_prix@naver.com>
2020-05-15 07:29:03 +0000

    Change build option

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 src=${1:-"main.cpp"}
 mkdir -p ./bin >/dev/null 2>&1
-if [ "$2" == "RELEASE" ]; then
+if [ "$2" == "R" ]; then
 	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./includes -o ./bin/a.out $src
 else
 	g++ -iquote ./includes -ggdb3 -O0 -D DEBUG -Wall -std=c++17 -fconcepts -o ./bin/a.out $src
