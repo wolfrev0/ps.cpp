cc35b2b68d7c0ab47391e8399312a54854af04c0
0xc0def <lobo_prix@naver.com>
2020-06-02 12:16:21 +0900

    Upd Trie

--- incl/Trie.h
+++ incl/Trie.h
@@ -1,15 +1,16 @@
 #pragma once 
 #include "Core.h"
 
-template<int n>
+template<int n, char base>
 struct Trie{
 	Trie(char ch):ch(ch),cnt(0){}
-	Trie* next(char c){if(!a[c])a[c]=new Trie<n>(c);return a[c];}
+	Trie* next(char c){c-=base;if(!a[c])a[c]=new Trie<n,base>(c);return a[c];}
 	void add(auto s, auto e){
 		cnt++;
 		if(s==e)return;
 		next(*s)->add(s+1,e);
 	}
+	void add(string s){add(all(s));}
 	char ch;
 	int cnt;
 private:
