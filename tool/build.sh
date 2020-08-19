330921cb450bcf96c338511184d688125d44646c
0xc0def <lobo_prix@naver.com>
2020-08-19 12:59:41 +0900

    tmp

--- tool/build.sh
+++ tool/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 src=${1:-"main.cpp"}
 mkdir -p ./bin >/dev/null 2>&1
-if [ "$2" == "R" ]; then
-	g++ -std=c++17 -O3 -Wall -fconcepts -iquote ./incl -o ./bin/a.out $src
-else
-	g++ -iquote ./incl -ggdb3 -O0 -D DEBUG=1 -Wall -std=c++17 -fconcepts -o ./bin/a.out $src
+if [ "$2" == "P" ]; then #preprocess
+	#g++ -iquote ./incl -std=c++17 -MM main.cpp | 
+	#sed -i 's%#include <%//PROTECTED #include <%g' $(find . -name '*.[hc]pp')
+else if [ "$2" == "R" ]; then #release
+	g++ -iquote ./incl -std=c++17 -O3 -Wall -fconcepts -o ./bin/a.out $src
+else #debug
+	g++ -iquote ./incl -std=c++17 -O0 -Wall -fconcepts -o ./bin/a.out -ggdb3 -D DEBUG=1  $src
 fi
 ./bin/merger.out $src ./incl
