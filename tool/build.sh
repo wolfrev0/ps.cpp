b0ae840eb817fd8f7cb03d33ca9f39d63c936fcd
0xc0def <lobo_prix@naver.com>
2020-08-25 15:53:10 +0900

    build upd

--- tool/build.sh
+++ tool/build.sh
@@ -21,8 +21,8 @@ function f(){
 	if (( 1 < $(echo $arr | wc -w | cat) )); then
 		for i in $arr
 		do
-			echo $(awk -F'"' '/#include *"/{print "incl/"$2, FILENAME}' $i)
-			z=$(awk -F'"' '/#include *"/{print "incl/"$2, "incl/"$2}' $i)
+			echo $(awk -F'"' '/^#include *"/{print "incl/"$2, FILENAME}' $i)
+			z=$(awk -F'"' '/^#include *"/{print "incl/"$2, "incl/"$2}' $i)
 			z=$(echo $z | cut -d ' ' -f2- | rev | cut -d ' ' -f2- | rev)
 			if (( 1 < $(echo $z | wc -w | cat) )); then
 				echo $z
