feaa4e7aa72bd59a900f791c8b2849573832e308
0xc0def <lobo_prix@naver.com>
2020-08-27 17:08:26 +0900

    .

--- tplscpc.cpp
+++ tplscpc.cpp
@@ -3,7 +3,7 @@
 #include "PrettyIO.h"
 //#include "PrettyDebug.h"
 
-#define int i64//MLE?
+//#define int i64//MLE?
 #define endl '\n'//interactive?
 using pint = pair<int,int>;
 using tint = tuple<int,int,int>;
