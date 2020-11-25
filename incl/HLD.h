907de3a68c7ba1c96523c833a25ef9b82fed4bd1
0xc0def <lobo_prix@naver.com>
2020-11-25 13:41:08 +0900

    HLD Refactoring

--- incl/HLD.h
+++ incl/HLD.h
@@ -7,18 +7,8 @@ struct HLD: public RootedTree<T>{
 	HLD(const Arr<pair<int,T>>& pi): RT(pi), cn(n), top(n){
 		int ccn=0;
 		dfs_hld(r, ccn);
-		ord=RT::pre();
-		top[0]=r;
+		ord=RT::pre(), top[0]=r;
 	}
-
-	int lca(int a, int b){
-		while(cn[a]!=cn[b]){
-			if(dpt[top[cn[a]]]<dpt[top[cn[b]]]) b=p[top[cn[b]]].fi;
-			else a=p[top[cn[a]]].fi;
-		}
-		return dpt[a]<dpt[b]?a:b;
-	}
-
 	void dfs_hld(int x, int& ccn){
 		if(sz(ch[x]))
 			swap(ch[x].front(), *max_element(all(ch[x]),[this](auto a, auto b){return this->tsz[a.fi]<this->tsz[b.fi];}));
@@ -29,18 +19,19 @@ struct HLD: public RootedTree<T>{
 			dfs_hld(ch[x][i].fi, ccn);
 		}
 	}
-
-	void iter(int a, int b, const Func<void(int,int)>& f, bool vtxw){
-		while(cn[a]!=cn[b]){
+	int iter(int a, int b, const Func<void(int,int)>& f, bool vtxw){
+		while(cn[a]!=cn[b])
 			if(dpt[top[cn[a]]]<dpt[top[cn[b]]])
-				f(ord[top[cn[b]]],ord[b]+1), b=p[top[cn[b]]].fi;
+				f(ord[top[cn[b]]],ord[b]+1),b=p[top[cn[b]]].fi;
 			else
-				f(ord[top[cn[a]]],ord[a]+1), a=p[top[cn[a]]].fi;
-		}
-		if(dpt[a]>dpt[b]) swap(a,b);
+				f(ord[top[cn[a]]],ord[a]+1),a=p[top[cn[a]]].fi;
+		if(dpt[a]>dpt[b])
+			swap(a,b);
 		f(ord[a]+!vtxw,ord[b]+1);
+		return a;
 	}
-	
+	int lca(int a, int b){ return iter(a,b,[&](int _, int __){},true); }
+
 	Arr<int> cn,top,ord;//chain number, top of the chain, vtx to preord
 	using RT=RootedTree<T>; using RT::n; using RT::r; using RT::p; using RT::ch; using RT::dpt;
 };
