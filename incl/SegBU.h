e48e4b7e1195577e372887b847791ffb10791b2f
0xc0def <lobo_prix@naver.com>
2020-11-26 16:53:47 +0900

    SegBU Refactoring

--- incl/SegBU.h
+++ incl/SegBU.h
@@ -6,8 +6,8 @@ template<class T, class Q, class U>
 struct SegBU{
 	SegBU(int n=0):n(n), tr(2*n,Q::id()){}
 	void upd(int i, T val){
-		i+=n, tr[i]=U::f(tr[i],val), i/=2;
-		for(;i;i>>=1)
+		i+=n, tr[i]=U::f(tr[i],val);
+		while(i>>=1)
 			tr[i]=Q::f(tr[i*2],tr[i*2+1]);
 	}
 	T q(int s, int e) {
