c4fa9a8df70a10f20d44320e2679d8cb5f1134b4
0xc0def <lobo_prix@naver.com>
2020-12-01 12:08:17 +0900

    XArr Upd

--- incl/XArr.h
+++ incl/XArr.h
@@ -1,18 +1,12 @@
 #pragma once
 #include "Core.h"
 
-template<typename T>
-struct XArr{
-	XArr(int n=0, int offset=1, T init=T()):arr(n, init), o(offset){}
-	auto& front()const{return arr.front();}
-	auto& back()const{return arr.back();}
-	auto begin()const{return arr.begin();}
-	auto end()const{return arr.end();}
-	T& operator[](int i){return arr[i+o];}
-	//const T& operator[](int i)const{return arr[i+o];}
-	void push_back(const T& x){arr.push_back(x);}
-	void emplace_back(T&& x){arr.emplace_back(x);}
-	auto erase(auto s, auto e){return arr.erase(s,e);}
-	Arr<T> arr;
+template<class T>
+struct XArr: public Arr<T>{
+	XArr(int n=0, T init=T(), int offset=1):Arr<T>(n, init), o(offset){}
+	constexpr T& operator[](int i){return Arr<T>::operator[](i+o);}
+	constexpr const T& operator[](int i)const{return Arr<T>::operator[](i+o);}
+	constexpr T& at(int i){return *this[i];}
+	constexpr const T& at(int i)const{return *this[i];}
 	int o;
 };
