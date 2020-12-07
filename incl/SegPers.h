5a5eb58f7ff212e4c162d0a4556a223e4630afca
0xc0def <lobo_prix@naver.com>
2020-12-07 17:47:45 +0900

    PST UPD

--- incl/SegPers.h
+++ incl/SegPers.h
@@ -1,61 +1,55 @@
-#pragma once
 #include "Core.h"
+#include "FastIO.h"
+#include "PrettyIO.h"
+//#include "PrettyDebug.h"
+#include "XArr.h"
 
-template<class T, class F, class U=T>
-struct SegPers{
-	struct Node{
-		Node *l=0, *r=0;
-		T val=F::id();
-	};
-
-	SegPers(int n):n(n){}
-	void build(Node* cur){ Arr<T> tmp(n); build(cur, tmp); }
-	void build(Node* cur, const Arr<T>& a){ build(cur, 0, n, a); }
-	T q(Node* tr, int s, int e){ return q(tr, 0, n, s, e); }
-	Node *upd(Node* tr, int idx, U val){
-		auto ret=new Node();
-		upd(ret, tr, 0, n, idx, val);
-		return ret;
-	}
-private:
-	int n;
-
-	void build(Node* cur, int s, int e, const Arr<T>& a){
-		if(e-s==1){
-			F::upd(cur->val,a[s],s,e);
-			return;
-		}
-
-		int m=(s+e)/2;
-		cur->l=new Node();
-		cur->r=new Node();
-		build(cur->l,s,m,a);
-		build(cur->r,m,e,a);
-		cur->val=F::q(cur->l->val, cur->r->val);
-	}
-
-	void upd(Node* cur, Node* bs, int s, int e, int idx, U v){
-		if(e<=idx or idx<s)
-			return;
-		if(e-s==1){
-			F::upd(cur->val,v,s,e);
-			return;
-		}
-
-		int m=(s+e)/2;
-		cur->l= idx<m?new Node():bs->l;
-		cur->r= idx<m?bs->r:new Node();
-		upd(cur->l, bs->l, s, m, idx, v);
-		upd(cur->r, bs->r, m, e, idx, v);
-		cur->val=F::q(cur->l->val, cur->r->val);
-	}
-
-	T q(Node* cur, int cs, int ce, int s, int e){
-		if(ce<=s or e<=cs or e<=s)
-			return F::id();
-		if(s<=cs and ce<=e)
-			return cur->val;
-		int m=(cs+ce)/2;
-		return F::q(q(cur->l, cs, m, s, e), q(cur->r, m, ce, s, e));
-	}
+struct A{
+	static A* id;
+	int i=-2,c=0;
+	A *l=id,*r=id;
 };
+A* A::id=new A{};
+auto id=A::id->l=A::id->r=A::id;
+
+int _upd(int s, int e, A* x, A*& y, int i, int v, int idx){
+	int m=(s+e)>>1;
+	if(y==id) y=new A{idx};
+	if(i<=s and e<=i+1) return y->c=x->c+v;
+	if(e<=i or i+1<=s) return y=x,y->c;
+	return y->c=_upd(s,m,x->l,y->l,i,v,idx)+_upd(m,e,x->r,y->r,i,v,idx);
+}
+int _q1(int s, int e, A* x, int k, int idx, int bit){
+	int m=(s+e)>>1;
+	if(!~bit) return s;
+	if(k>>bit&1) return x->l->i>=idx?_q1(s,m,x->l,k,idx,bit-1):_q1(m,e,x->r,k,idx,bit-1);
+	return x->r->i>=idx?_q1(m,e,x->r,k,idx,bit-1):_q1(s,m,x->l,k,idx,bit-1);
+}
+int _q2(int s, int e, A* x, int k){
+	int m=(s+e)>>1;
+	if(e-s==1) return x->c;
+	return k<m?_q2(s,m,x->l,k):x->l->c+_q2(m,e,x->r,k);
+}
+int _q3(int s, int e, A* x, A* y, int k){
+	int m=(s+e)>>1;
+	if(e-s==1) return s;
+	return y->l->c-x->l->c>=k?_q3(s,m,x->l,y->l,k):_q3(m,e,x->r,y->r,k-y->l->c+x->l->c);
+}
+A* upd(A* x, int i, int v, int idx){ A* y=id; _upd(0,1<<19,x,y,i,v,idx); return y; }
+int q1(A* x, int k, int idx){ return _q1(0,1<<19,x,k,idx,18); }
+int q2(A* x, int k){ return _q2(0,1<<19,x,k); }
+int q3(A* x, A* y, int k){ return _q3(0,1<<19,x,y,k); }
+
+signed main(){
+	XArr<A*> a={new A{-1}};
+	CIN(int,m);
+	rep(_,m){
+		CIN(int,t);
+		if(t==1){ CIN(int,x); a.emplb(upd(a.back(),x,1,sz(a)-1)); }
+		if(t==2){ CIN(int,l,r,x); l--,r--; cout<<q1(a[r],x,l)<<endl; }
+		if(t==3){ CIN(int,k); a.erase(a.end()-k,a.end()); }
+		if(t==4){ CIN(int,l,r,x); l--,r--; cout<<q2(a[r],x)-q2(a[l-1],x)<<endl; }
+		if(t==5){ CIN(int,l,r,k); l--,r--; cout<<q3(a[l-1],a[r],k)<<endl; }
+	}
+}
+//https://www.acmicpc.net/problem/13538
