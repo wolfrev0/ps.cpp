eb7889e41dc8d464a1e490e849c4578e3c6ca2fd
0xc0def <lobo_prix@naver.com>
2020-12-07 17:46:43 +0900

    XArr Upd

--- incl/XArr.h
+++ incl/XArr.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "Core.h"
 
-template<class T>
+template<class T, int bias=1>
 struct XArr: public Arr<T>{
-	XArr(int n=0, T init=T(), int offset=1):Arr<T>(n, init), o(offset){}
-	constexpr T& operator[](int i){return Arr<T>::operator[](i+o);}
-	constexpr const T& operator[](int i)const{return Arr<T>::operator[](i+o);}
-	constexpr T& at(int i){return *this[i];}
-	constexpr const T& at(int i)const{return *this[i];}
-	int o;
+	XArr(int n=0, T init=T()):Arr<T>(n, init){}
+	XArr(initializer_list<T> il):Arr<T>(il){}
+	T& operator[](int i){return Arr<T>::operator[](i+bias);}
+	const T& operator[](int i)const{return Arr<T>::operator[](i+bias);}
+	T& at(int i){return *this[i];}
+	const T& at(int i)const{return *this[i];}
 };
