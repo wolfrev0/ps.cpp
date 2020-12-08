5cf4569214577ddb565729b3ed42bc2b9765d7ca
0xC0DEF <lobo_prix@naver.com>
2020-12-08 23:56:49 +0900

    Update PST.h

--- incl/PST.h
+++ incl/PST.h
@@ -4,13 +4,13 @@
 
 template<class T, class Q, class U, int n>
 struct PST{
-	PST(T v):v(v),l(id()),r(id()){}
-	PST(PST *l, PST *r):v(Q::f(l->v,r->v)),l(l),r(r){}
+	static PST* id(){ static PST* a; if(!a)a=new PST(),a->l=a->r=a; return a; }
 	PST* upd(int i, T val){ return upd(0,n,i,val); }
 	T q(int s, int e){ return q(0,n,s,e); }
-	static PST* id(){ static PST* a; if(!a)a=new PST(),a->l=a->r=a; return a; }
 private:
 	PST():v(Q::id()){};
+	PST(T v):v(v),l(id()),r(id()){}
+	PST(PST *l, PST *r):v(Q::f(l->v,r->v)),l(l),r(r){}
 	PST* upd(int cs, int ce, int i, T val){
 		int cm=(cs+ce)>>1;
 		if(i<=cs and ce<=i+1) return new PST(U::f(v,val));
