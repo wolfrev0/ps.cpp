a109c4f04edde19b0d8b1d03cffde06301ddf4fc
0xc0def <lobo_prix@naver.com>
2021-01-01 02:04:29 +0900

    .

--- incl/Carr.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Core.h"
-
-template<class T> struct Carr:public vector<T>{
-	using P=vector<T>;
-	Carr(int n=0, T v=T()):P(n, v){}
-	T& operator[](int idx){return P::operator[]((idx%sz(*this)+sz(*this))%sz(*this));}
-};
