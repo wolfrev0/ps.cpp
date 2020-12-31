94431d26dbf2e8a34167bcbd1eae215bffaac279
0xc0def <lobo_prix@naver.com>
2021-01-01 04:04:29 +0900

    Build bugfix and upd

--- tool/build.sh
+++ tool/build.sh
@@ -21,9 +21,8 @@ function f(){
 	if (( 1 < $(echo $arr | wc -w | cat) )); then
 		for i in $arr
 		do
-			echo $(awk -F'"' '/^#include *"/{print "incl/"$2, FILENAME}' $i)
+			echo $(awk -F'"' '/^#include *"/{print FILENAME, "incl/"$2}' $i)
 			z=$(awk -F'"' '/^#include *"/{print "incl/"$2, "incl/"$2}' $i)
-			z=$(echo $z | cut -d ' ' -f2- | rev | cut -d ' ' -f2- | rev)
 			if (( 1 < $(echo $z | wc -w | cat) )); then
 				echo $z
 			fi
@@ -33,5 +32,5 @@ function f(){
 	fi
 }
 if ! [[ "$2" =~ ?*.h ]]; then #header
-	awk '//' $(f) | grep -Ev '#include *"|#pragma once' > submit.cpp
+	awk '//' $(f|tac) | grep -Ev '#include *"|#pragma once' > submit.cpp
 fi
