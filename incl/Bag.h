81f8fe2c6f5103954e425c0eddf5f84d1b944284
0xc0def <lobo_prix@naver.com>
2021-01-15 07:00:36 +0900

    indent mismatch correction

--- incl/Bag.h
+++ incl/Bag.h
@@ -7,22 +7,22 @@ struct Bag{
 		N* n=nullptr;
 	};
 	N *h=new N, *t=h;
-    int cnt=0;
+	int cnt=0;
 
-    Bag(){}
-    Bag(initializer_list<T> il):cnt(sz(il)){
-        for(auto i:il)
-            add(i);
-    }
+	Bag(){}
+	Bag(initializer_list<T> il):cnt(sz(il)){
+		for(auto i:il)
+			add(i);
+	}
 
 	void add(T x){t=t->n=new N{x}; cnt++;}
 	void merge(Bag a){t->n=a.h->n; t=a.t; a.h->n=nullptr; a.t=a.h; cnt+=a.cnt; a.cnt=0;}
-    int size()const{return cnt;}
-    void iter(Func<void(T&)> f){
-        N* c=h->n;
-        while(c)
-            f(c->d), c=c->n;
-    }
-    Arr<T> toArr(){Arr<T> ret; iter([&ret](T x){ret.pushb(x);}); return ret;}
-    bool operator<(Bag a)const{return false;}//for pair compare
+	int size()const{return cnt;}
+	void iter(Func<void(T&)> f){
+		N* c=h->n;
+		while(c)
+			f(c->d), c=c->n;
+	}
+	Arr<T> toArr(){Arr<T> ret; iter([&ret](T x){ret.pushb(x);}); return ret;}
+	bool operator<(Bag a)const{return false;}//for pair compare
 };
--- incl/DST.h
+++ incl/DST.h
@@ -2,42 +2,42 @@
 #include "Core.h"
 
 struct Node{
-    signed cs,ce,v,w;
-    Node *l,*r;
+	signed cs,ce,v,w;
+	Node *l,*r;
 
-    void upd(signed s, signed e, char x){
-        if(ce<=s or e<=cs)return;
-        if(s<=cs and ce<=e){
-            w+=x;
-            v=w?ce-cs:(l?l->v:0)+(r?r->v:0);
-            return;
-        }
-        signed m=(cs+ce)/2;
-        if(!l)l=new Node{cs,m};
-        if(!r)r=new Node{m,ce};
-        l->upd(s,e,x), r->upd(s,e,x);
-        v=w?ce-cs:l->v+r->v;
-    }
+	void upd(signed s, signed e, char x){
+		if(ce<=s or e<=cs)return;
+		if(s<=cs and ce<=e){
+			w+=x;
+			v=w?ce-cs:(l?l->v:0)+(r?r->v:0);
+			return;
+		}
+		signed m=(cs+ce)/2;
+		if(!l)l=new Node{cs,m};
+		if(!r)r=new Node{m,ce};
+		l->upd(s,e,x), r->upd(s,e,x);
+		v=w?ce-cs:l->v+r->v;
+	}
 } root{0,signed(1e9+9)};
 
 int solution(vector<vector<signed> > rects)
 {
-    map<signed,Arr<tint>> a;
-    for(auto& i:rects){
-        a[i[0]].emplb(i[1],i[3],1);
-        a[i[2]].emplb(i[1],i[3],-1);
-    }
+	map<signed,Arr<tint>> a;
+	for(auto& i:rects){
+		a[i[0]].emplb(i[1],i[3],1);
+		a[i[2]].emplb(i[1],i[3],-1);
+	}
 
-    int ans=0;
-        signed px=0;
-        signed pv=0;
-    for(auto [x,ev]:a){
-        for(auto [y1,y2,d]:ev)
-            root.upd(y1,y2,d);
-        ans+=pv*int(x-px);
-        px=x, pv=root.v;
-    }
-    return ans;
+	int ans=0;
+		signed px=0;
+		signed pv=0;
+	for(auto [x,ev]:a){
+		for(auto [y1,y2,d]:ev)
+			root.upd(y1,y2,d);
+		ans+=pv*int(x-px);
+		px=x, pv=root.v;
+	}
+	return ans;
 }
 
 struct A{
