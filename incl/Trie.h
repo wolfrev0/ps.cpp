5616bcbffaf198e6984740882efedd4c88fe8c45
0xC0DEF <loboprix9852@gmail.com>
2021-04-15 07:29:03 +0900

    Bitrie

--- incl/Trie.h
+++ incl/Trie.h
@@ -21,5 +21,31 @@ template<class T, int n, char base = 0> struct Trie {
 	char ch;
 	int cnt;
 	Trie *p, *a[n]{};
-	T val;
+};
+
+//MSB to LSB, maxval=1e9(30bit)
+struct Bitrie {
+	Bitrie(char bit=0, Bitrie* p=0) : bit(bit), cnt(0), p(p) {}
+	Bitrie* next(char bit) {
+		if(!a[bit]) a[bit] = new Bitrie(bit, this);
+		return a[bit];
+	}
+	Bitrie* add(int x, int i=30){
+		cnt++;
+		return !~i ? this : next(x>>i&1)->add(x,i-1);
+	}
+	void rem(int x, int i=30) {
+		cnt--;
+		if(~i) next(x>>i&1)->rem(x,i-1);
+	}
+
+	int mex(int x, int i=30){
+		if(i==-1) return 0;
+		if(next(x>>i&1)->cnt<(1<<i)) return next(x>>i&1)->mex(x,i-1);
+		else return (1<<i)+next(x>>i&1^1)->mex(x,i-1);
+	}
+
+	char bit;
+	int cnt;
+	Bitrie *p, *a[2]{};
 };
