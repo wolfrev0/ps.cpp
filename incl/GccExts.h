d6baf8f14cb8e373540e25adda20695cb76f4318
0xC0DEF <loboprix9852@gmail.com>
2021-04-22 06:27:12 +0900

    Multixet LB/UB

--- incl/GccExts.h
+++ incl/GccExts.h
@@ -30,7 +30,7 @@ template<class T> struct Multixet {
 	}
 	void insert(const T& v) { s.insert({v, counter++}); }
 	void erase(const typename Xet<pair<T, int>>::iterator& it) { s.erase(it); }
-	int order_of_key(const T& v) const { return s.order_of_key({v, counter}); }
+	int order_of_key(const T& v, bool ub=true) const { return s.order_of_key({v, ub?counter:0}); }
 	auto find_by_order(int ord) const { return s.find_by_order(ord); }
 	int count(const T& v) const { return order_of_key(v) - order_of_key(v - 1); }
 	int size() const { return sz(s); }
