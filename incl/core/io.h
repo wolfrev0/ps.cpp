aa792b09d8ce62efee8cf1444fd0dd574b6e318f
0xC0DEF <loboprix9852@gmail.com>
2021-05-30 14:25:28 +0900

    io refactoring

--- incl/core/io.h
+++ incl/core/io.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "core/base.h"
+static_assert(!INTERACTIVE);
 
-#if !(DEBUG)&&!(INTERACTIVE)
 struct FIO{
 	static const int SZ=1<<22;
 	unsigned cnt=0;char a[SZ],*p;
@@ -15,13 +15,14 @@ struct FIO{
 	FIO& operator>>(T& n){ignore_wsc();n=0;char neg=false;if(get()=='-')neg=true,pop();while('0'<=get()&&get()<='9')n=n*10+pop()-'0';if(neg)n*=-1;return *this;}
 	FIO& operator>>(char& c){ignore_wsc();c=pop();return *this;}
 	FIO& operator>>(string& s){ignore_wsc();s.clear();while(get()!=' '&&get()!='\n')s.pushb(pop());return *this;}
-}fastcin;
-#define cin fastcin
-#endif
+};
+struct PrettyIO:FIO{
+	template<class T=int>T tok(){T x;*this>>x;return x;}
+	template<class T=int>Arr<T> toks(int n){Arr<T> a(n);for(auto& i:a)*this>>i;return a;}
+	template<class... A> void _read(A&...a){((*this>>a),...);}
+}_pcin;
+#define cin _pcin
 
 template<class T> ostream& operator<<(ostream& s,const Arr<T>& a){for(auto i:a)cout<<i<<' ';return s;}
-template<class T=int>T geti(){T x;cin >> x;return x;}
-template<class T=int>Arr<T> getis(int n){Arr<T> a(n);for(auto& i:a)cin>>i;return a;}
-template<class... A> void _read(A&...a){((cin>>a),...);}
-#define read(T,...) T __VA_ARGS__;_read(__VA_ARGS__);
-template<class... A> void print(A...a){((cout<<a<<' '),...,(cout<<endl));}
+#define READ(T,...) T __VA_ARGS__;cin._read(__VA_ARGS__);
+template<class... A> void PRINT(A...a){((cout<<a<<' '),...,(cout<<endl));}
