4a939bbff30b7523ae2c92a872878a4bb71d5f00
0xC0DEF <loboprix9852@gmail.com>
2021-06-06 20:44:01 +0900

    cin string bugfix

--- incl/core/fastio.h
+++ incl/core/fastio.h
@@ -12,7 +12,7 @@ struct FastCIN{
 	operator bool(){return get();}
 	FastCIN& operator>>(int& n){ignore_wsc();n=0;char neg=false;if(get()=='-')neg=true,pop();while('0'<=get()&&get()<='9')n=n*10+pop()-'0';if(neg)n*=-1;return *this;}
 	FastCIN& operator>>(char& c){ignore_wsc();c=pop();return *this;}
-	FastCIN& operator>>(string& s){ignore_wsc();s.clear();while(get()!=' '&&get()!='\n')s.pushb(pop());return *this;}
+	FastCIN& operator>>(string& s){ignore_wsc();s.clear();while(get()!=' '&&get()!='\n'&&get())s.pushb(pop());return *this;}
 	FastCIN& operator>>(f64& n){ignore_wsc();string s;while('0'<=get()&&get()<='9'||get()=='.'||get()=='-')s.pushb(pop());n=stod(s);return *this;}
 };
 struct TokCIN:FastCIN{
