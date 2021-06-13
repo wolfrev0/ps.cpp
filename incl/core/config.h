e646fe8b48a8f68f623f71743d5a626932fd0155
0xC0DEF <loboprix9852@gmail.com>
2021-06-13 22:15:58 +0900

    config.h noignore

---
+++ incl/core/config.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "core/std.h"
+
+//Don't use at interactive problem
+#include "core/fastio.h"
+#define endl '\n'
+
+#define int i64
+#define FP_EPS 1e-11
+#define COUT_FP 12
+// #define CPP20
