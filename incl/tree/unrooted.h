22bb7affd473b4fb447eaea8645d754b62b69eb4
0xC0DEF <loboprix9852@gmail.com>
2021-08-04 07:59:15 +0900

    centroid decomposition

--- incl/tree/unrooted.h
+++ incl/tree/unrooted.h
@@ -34,6 +34,37 @@ template<class T> struct Tree{
 		return res;
 	}
 
+	//Usage: https://www.acmicpc.net/submit/22360/31799657
+	Arr<int> centroid_ord()const{
+		Arr<char> av(n,true);
+		Arr<int> tsz(n);
+		func(int,upd_tsz,int x,int p){
+			tsz[x]=1;
+			for(auto [i,_]:g[x])
+				if(av[i] and i!=p)
+					tsz[x]+=upd_tsz(i,x);
+			return tsz[x];
+		};
+		func(int,centroid,int x,int p,int sztot){
+			for(auto [i,_]:g[x])
+				if(av[i] and i!=p and tsz[i]*2>=sztot)
+					return centroid(i,x,sztot);
+			return x;
+		};
+		Arr<int> ret;
+		func(void,f,int x){
+			upd_tsz(x,x);
+			x=centroid(x,x,tsz[x]);
+			ret.emplb(x);
+			av[x]=false;
+			for(auto [i,_]:g[x])
+				if(av[i])
+					f(i);
+		};
+		f(0);
+		return ret;
+	}
+
 	int n;
 	Arr<Arr<pair<int, T>>> g;
 
