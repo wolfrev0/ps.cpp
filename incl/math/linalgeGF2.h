dd850dd35d9cccc8ffeccfcf484ae2f75e89f938
0xC0DEF <loboprix9852@gmail.com>
2021-08-06 06:52:37 +0900

    matgf2 upd

--- incl/math/linalgeGF2.h
+++ incl/math/linalgeGF2.h
@@ -1,88 +1,70 @@
 #pragma once
 #include "core/base.h"
 
-template<int m> struct MatGF2 {
+//NOTE: n*m행렬이다. m*n행렬이 아님(코드상으로는 <m> (n)형태라 헷갈리니 조심)
+template<int m> struct MatGF2{
 	const int n;
 	Arr<bitset<m>> a;
-
-	MatGF2(int n, bool one = false) : n(n), a(n) {
-		if(!one) return;
-		for(int i = 0; i < min(n, m); i++)
-			for(int j = 0; j < min(n, m); j++) a[i][j] = 1;
+	MatGF2(int n):n(n),a(n){}
+	Arr<int> operator*(const bitset<m>& v)const{
+		//NOTE: M*v 말고 v*M이 필요한 경우 전치시키면 됨
+		Arr<int> ret(n);
+		for(int i=0;i<n;i++)
+			ret[i]=(a[i]&v).count()%2;
+		return ret;
 	}
-
-	template<int rm> MatGF2 operator*(const MatGF2<rm>& r) const {
-		assert(m == r.n);
-		MatGF2 ret(n, rm);
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++)
-				if(a[i][j]) ret.a[i] ^= r.a[j];
+	MatGF2 inv(){
+		if(n>m)
+			throw "No MatInv";
+		MatGF2<m*2> tmp(n);
+		for(int i=0;i<n;i++)
+			for(int j=0;j<n;j++)
+				tmp.a[i][j]=a[i][j];
+		for(int i=0;i<n;i++)
+			tmp.a[i][n+i]=1;
+		if(tmp.RREF(n)!=n)
+			throw "No MatInv";
+		MatGF2 ret(n);
+		for(int i=0;i<n;i++)
+			for(int j=0;j<n;j++)
+				ret.a[i][j]=tmp.a[i][n+j];
 		return ret;
 	}
 
-	void transpose() {}
-
-	MatGF2 sub(int r, int c, int rn, int rm) const {
-		MatGF2 ret(rn, rm);
-		for(int i = 0; i < rn; i++)
-			for(int j = 0; j < rm; j++) ret.a[i][j] = a[r + i][c + j];
-		return ret;
-	}
-
-	void REF(bool pv_fix) {
-		int pi = 0;
-		int i = 0;
-		for(; i < n; i++) {
-			while(pi < m && a[i][pi] == 0) {
-				if(!pv_fix) {
-					int j = i + 1;
-					for(; j < n; j++)
-						if(a[j][pi]) break;
-					if(j < n) {
-						swap(a[i], a[j]);
-						break;
-					}
+	int REF(bool pv_fix){
+		int pi=0,i=0;
+		for(;i<n;i++){
+			while(pi<m&&a[i][pi]==0){
+				if(!pv_fix){
+					int j=i+1;
+					for(;j<n;j++)if(a[j][pi])break;
+					if(j<n){swap(a[i],a[j]);break;}
 				}
 				pi++;
 			}
-			if(pi == m) break;
-			if(a[i][pi])
-				for(int j = i + 1; j < n; j++)
-					if(a[j][pi]) a[j] ^= a[i];
+			if(pi==m)break;
+			if(a[i][pi])for(int j=i+1;j<n;j++)if(a[j][pi])a[j]^=a[i];
 			pi++;
 		}
-		rank = i;
+		return rank=i;
 	}
-	int rank = -1;
-
-	void RREF(int rm) {
-		REF(false);
-
-		flipX(rank);
-		flipY(rm);
-		REF(true);
-		flipY(rm);
-		flipX(rank);
-	}
-
-	void flipX(int rn) {
-		for(int i = 0; i < rn / 2; i++) swap(a[i], a[rn - 1 - i]);
-	}
-
-	void flipY(int rm) {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < rm / 2; j++) {
-				bool z = a[i][j];
-				a[i][j] = a[i][rm - 1 - j];
-				a[i][rm - 1 - j] = z;
+	int RREF(int rm){REF(false),flipX(rank),flipY(rm),REF(true),flipY(rm),flipX(rank);return rank;}
+	void flipX(int rn){for(int i=0;i<rn/2;i++)swap(a[i],a[rn-1-i]);}
+	void flipY(int rm){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<rm/2;j++){
+				bool z=a[i][j];
+				a[i][j]=a[i][rm-1-j],a[i][rm-1-j]=z;
 			}
 		}
 	}
+private:
+	int rank=-1;
 };
 
-template<int m> ostream& operator<<(ostream& s, const MatGF2<m>& mat) {
-	for(int i = 0; i < mat.n; i++) {
-		for(int j = 0; j < m; j++) s << mat.a[i][j] << ' ';
+template<int m> ostream& operator<<(ostream& s, const MatGF2<m>& mat){
+	for(int i=0;i<mat.n;i++){
+		for(int j=0;j<m;j++) s << mat.a[i][j] << ' ';
 		s << endl;
 	}
 	return s;
@@ -93,14 +75,14 @@ template<int m> ostream& operator<<(ostream& s, const MatGF2<m>& mat) {
 //((staircase basis, basis coefficient),selected basis indices)
 // staircase basis: has some good property
 // basis coefficient and selected basis indices: for reconstruct
-pair<Arr<pint>, Arr<int>> xor_basis(const Arr<int>& a) {
-	Arr<pint> r(64, {-1, -1});  // descending
+pair<Arr<pint>, Arr<int>> xor_basis(const Arr<int>& a){
+	Arr<pint> r(64,{-1, -1}); // descending
 	Arr<int> bi;
-	for(int i = 0; i < sz(a); i++) {
-		int x = a[i], xc = 0;
+	for(int i=0;i<sz(a);i++){
+		int x=a[i], xc=0;
 		for(auto [b, bc] : r)
-			if(~b and x > (x ^ b)) x ^= b, xc ^= bc;
-		if(x) r[63 - lg2f(x)] = {x, xc ^ (1ll << sz(bi))}, bi.pushb(i);
+			if(~b and x > (x ^ b)) x^=b, xc^=bc;
+		if(x) r[63-lg2f(x)]={x, xc ^ (1ll << sz(bi))}, bi.pushb(i);
 	}
-	return {move(r), move(bi)};
+	return{move(r), move(bi)};
 }
