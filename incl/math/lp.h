106174d54f319ffd0b970ec470969874a1b7bc25
0xb612f <0xb612f@gmail.com>
2021-12-18 06:52:32 +0900

    simplex upd

--- incl/math/lp.h
+++ incl/math/lp.h
@@ -7,9 +7,11 @@
 //numeric stability is sensitive by M
 //디버깅 노트
 //1. T=f64 해보기(정수값만 나오는거같아도 중간에 유리수나올때 있음)
-//2. M값 조절(답의 상한정도의 크기가 적절)
+//2. M값 조절(답의 상한정도의 크기가 적절. Road Times처럼 1일수도 있음)
+//3. eps값 최대한 크게(오차가 상당한편이라..Road Times에서 고생함)
 //듀얼후 리덕션한 결과값 primal로 복원하기
 //https://math.stackexchange.com/questions/1226186/recovering-the-optimal-primal-solution-from-dual-solution
+//왠지 모르겠지만 double속도가 더 빨라서 확실한 실수오차문제가 아니면 그냥 f64로 냅두는게 좋은듯
 template<class T=f64,int M>
 void dualize(Arr<Arr<T>> &a,Arr<T> &b,Arr<T>& obj){
 	int m=sz(a), n=sz(a[0]);
@@ -21,12 +23,12 @@ void dualize(Arr<Arr<T>> &a,Arr<T> &b,Arr<T>& obj){
 	for(auto& i:obj)i=-i;
 }
 template<class T=f64,int M>
-tuple<T,Arr<T>,Arr<T>> simplex(Arr<Arr<T>>& a,Arr<T>& b,Arr<T>& obj){
+tuple<T,Arr<T>,Arr<T>> simplex(Arr<Arr<T>> a,Arr<T> b,Arr<T> obj){
 	//return {maxval,argmax,dual_argmin}
 	int m=sz(a),n=sz(a[0]),s=0;
 	if(m>n){
 		dualize<T,M>(a,b,obj);
-		auto&& [x,y,z]=simplex<T,M>(a,b,obj);
+		auto&& [x,y,z]=simplex<T,M>(move(a),move(b),move(obj));
 		x*=-1;
 		swap(y,z);
 		return {move(x),move(y),move(z)};
@@ -45,7 +47,7 @@ tuple<T,Arr<T>,Arr<T>> simplex(Arr<Arr<T>>& a,Arr<T>& b,Arr<T>& obj){
 	for(int i=0;i<m;i++)
 		if(b[i]<0){
 			for(auto& j:a[i])j=-j;
-			for(auto& r:a)r.emplb(0);
+			for(auto& r:a)r.pushb(0);
 			a[i][-1]=-1,b[i]=-b[i],geq[i]=true,s++;
 		}
 
@@ -58,11 +60,17 @@ tuple<T,Arr<T>,Arr<T>> simplex(Arr<Arr<T>>& a,Arr<T>& b,Arr<T>& obj){
 	//z=f(x) == z-f(x)=0
 	for(auto &i:obj)i=-i;
 	obj[-2]=1;
-	a.emplb(obj);
-	
+	a.pushb(obj);
+
 	for(int i=0;i<m;i++)
 		elim(i,m,p[i]);
 	
+	for(int i=0;i<m;i++){
+		int j=rand()%m;
+		swap(a[i],a[j]);
+		swap(p[i],p[j]);
+	}
+
 	//now shape of a = (m+1)*(n+s+m+2)
 	while(true){
 		int ev=0,lvi=-1;
@@ -73,10 +81,17 @@ tuple<T,Arr<T>,Arr<T>> simplex(Arr<Arr<T>>& a,Arr<T>& b,Arr<T>& obj){
 			if(a[i][ev]>eps and (!~lvi or a[i][-1]/a[i][ev]<a[lvi][-1]/a[lvi][ev]))
 				lvi=i;
 		if(!~lvi) throw "unbounded";
-		for(int i=0;i<m+1;i++)elim(lvi,i,ev);
+		for(int i=0;i<m+1;i++)
+			elim(lvi,i,ev);
+
 		p[lvi]=ev;
 	}
-	//if(?) throw "infeasible"
+	//NOTE: 검증안된 뇌피셜 infeasible 판별(M을 매우 크게하면 동작하는거같다)
+	//확실한 방법1: 포기하고 2phase 사용(1phase가 infeasible체크임)
+	//확실한 방법2: Dual시켜 돌리기(unbounded는 판별되니까 infeasible로 뒤집혀서 체크가능,가로세로 크기 비슷할때만 가능)
+	// for(int i=n;i<n+s+m;i++)
+	// 	if(a[-1][i]>=M)
+	// 		throw "infeasible";
 	Arr<T> ans(n+s+m+2);
 	for(int i=0;i<m;i++)
 		ans[p[i]]=a[i][-1];
