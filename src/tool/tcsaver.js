51eea7d9073b1db1bb84503cb87712504e9d74bd
0xb612f <0xb612f@gmail.com>
2021-12-22 15:37:54 +0900

    testcase saver

---
+++ src/tool/tcsaver.js
@@ -0,0 +1,25 @@
+//Testcase Auto Downloader
+//Requirements: node(v14), competitive companion(web browser extension)
+//How to use
+//1. install [competitive companion] on web browser
+//2. run this server
+//3. click green (+) button on browser in problem page
+//4. build your code and run testtc.sh
+const app = require('express')();
+const bodyParser = require('body-parser');
+const port = 10043;
+app.use(bodyParser.json());
+app.post('/', (req, res) => {
+  const data = req.body;
+  console.log('['+data.name+'] parsed.');
+	fs = require('fs');
+	if(fs.existsSync('tc'))
+		fs.rmdirSync('tc',{recursive:true});
+	fs.mkdirSync('tc');
+	for(let i=0;i<data.tests.length;i++){
+		fs.writeFileSync('tc/'+i+'.in', data.tests[i].input);
+		fs.writeFileSync('tc/'+i+'.ans', data.tests[i].output);
+	}
+  res.sendStatus(200);
+});
+app.listen(port, err => {console.log('Server started.');});
