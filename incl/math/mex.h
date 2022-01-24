4228c801df5b749c7b9c0219b001c6604c2ad1ef
0xC0DEF <loboprix9852@gmail.com>
2022-01-24 23:58:03 +0900

    mex container

---
+++ incl/math/mex.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "core/base.h"
+#include "misc/gccext.h"
+
+//TODO: DynSeg+BinaryLifting해서 복잡도 log로 개선하기
+struct Mex{
+	pbds_map<int,int> a;
+	void add(int x){a[x]++;}
+	void del(int x){
+		auto it=a.find(x);
+		assert(it!=a.end());
+		if(!--(it->se))
+			a.erase(it);
+	}
+	int mex(){
+		int s=-1,e=sz(a);
+		while(e-s>1){
+			int m=(s+e)/2;
+			if((int)a.order_of_key(m+1)==m+1)s=m;//m까지 다 있음
+			else e=m;//m까지 빠진게 있음
+		}
+		return e;
+	}
+};
