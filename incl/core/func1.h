a6cc3919a359637ba3b97e27ca17f89a95fa5d37
0xC0DEF <loboprix9852@gmail.com>
2022-01-29 08:32:22 +0900

    print refactoring

--- incl/core/func1.h
+++ incl/core/func1.h
@@ -1,8 +1,9 @@
+//functions before include <arr.h>
 #pragma once
 #include "core/config.h"
 #include "core/abbr.h"
 
-//Handy Funcs
+//Math
 template<class T>int sz(const T& x){return x.size();}
 int fdiv(int a,int b){if(b<0)a=-a,b=-b;return (a>0)?a/b:(a-b+1)/b;}
 int cdiv(int a,int b){if(b<0)a=-a,b=-b;return (a>0)?(a+b-1)/b:a/b;}
@@ -11,7 +12,20 @@ i64 clg2(i64 x){return 64-__builtin_clzll(x-1);}
 int fsqrt(i64 n) {i64 i=1;while(i*i<=n)i++;return i-1;}
 int csqrt(i64 n) {i64 i=1;while(i*i<n)i++;return i;}
 template<class T>T sq(T x){return x*x;}
+template<class T>T inf(){return numeric_limits<T>::max()/2;}
+template<class T>T nan(){return inf<T>()+1;}
+#ifdef CONCEPT
+template<typename T> concept MemberInf=requires(T t){t.inf();};
+template<typename T> concept MemberNan=requires(T t){t.nan();};
+template<MemberInf T>T inf(){return T().inf();}
+template<MemberNan T>T nan(){return T().nan();}
+#endif
 
+//IO & misc
+template<char endch='\n',char sepch=' ',class... A>
+void print(A...a){((cout<<a<<sepch),...,(cout<<endch));}
+template<class T,class U>bool assmin(T& a,U&& b){return a>b?a=b,true:false;}
+template<class T,class U>bool assmax(T& a,U&& b){return a<b?a=b,true:false;}
 void WARN(bool cond,const char* str){
 #if DEBUG
 	static set<const char*> z;
@@ -25,11 +39,7 @@ void MUST(bool expr){
 		raise(SIGINT);
 #endif
 }
-template<class T>T inf(){return numeric_limits<T>::max()/2;}
-template<class T>T nan(){return inf<T>()+1;}
-#ifdef CONCEPT
-template<typename T> concept MemberInf=requires(T t){t.inf();};
-template<typename T> concept MemberNan=requires(T t){t.nan();};
-template<MemberInf T>T inf(){return T().inf();}
-template<MemberNan T>T nan(){return T().nan();}
-#endif
+#define ARG(...) __VA_ARGS__
+#define func(RetT,fname,...) function<RetT(__VA_ARGS__)> fname=[&](__VA_ARGS__)->RetT
+#define lam(expr,...) [&](__VA_ARGS__){return expr;}
+#define lamp(expr,...) [](__VA_ARGS__){return expr;}
--- incl/core/func2.h
+++ incl/core/func2.h
@@ -1,13 +1,7 @@
+//functions after include <arr.h>
 #pragma once
 #include "core/arr.h"
 
-#define ARG(...) __VA_ARGS__
-#define func(RetT,fname,...) function<RetT(__VA_ARGS__)> fname=[&](__VA_ARGS__)->RetT
-#define lam(expr,...) [&](__VA_ARGS__){return expr;}
-#define lamp(expr,...) [](__VA_ARGS__){return expr;}
-
-template<class T,class U>bool assmin(T& a,U&& b){return a>b?a=b,true:false;}
-template<class T,class U>bool assmax(T& a,U&& b){return a<b?a=b,true:false;}
 template<class T>int argmin(const Arr<T>& a){return min_element(a.begin(),a.end())-a.begin();}
 template<class T>int argmax(const Arr<T>& a){return max_element(a.begin(),a.end())-a.begin();}
 Arr<int> permuinv(const Arr<int>& a){Arr<int> r(sz(a));for(int i=0;i<sz(a);i++)r[a[i]]=i;return r;}
@@ -17,5 +11,3 @@ template<class T>map<T,Arr<int>> classify(const Arr<T>& a){
 		r[a[i]].push_back(i);
 	return r;
 }
-template<class... A,char sepch=' ',char endch='\n'>
-void PRINT(A...a){((cout<<a<<sepch),...,(cout<<endch));}
