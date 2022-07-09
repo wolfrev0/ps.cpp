41dea7f69c8862b2f0a3cfa912bb3b4dc149900d
0xb612f <0xb612f@gmail.com>
2022-07-09 10:56:43 +0900

    update example codes

---
+++ src/saved/line_point_matching/line_point_match_9576.cpp
@@ -0,0 +1,23 @@
+#include "core/base.h"
+#include "misc/ordering.h"
+
+void solve(){
+	auto [n,m]=input<int,int>();
+	auto a=ARR(n,0ll); iota(a.begin(),a.end(),1);
+	auto b=ARR(m,2,0ll); input(b);
+
+	int ans=0;
+	multiset<int> z(a.begin(),a.end());
+	sort(b.begin(),b.end(),lam(x[1]<y[1],auto& x,auto& y));
+	for(const auto& i:b){
+		auto it = z.lb(i[0]);
+		if(it!=z.end() and *it<=i[1])
+			ans++,z.erase(it);
+	}
+	println(ans);
+}
+signed main(){
+	for(int ti=1,t=get<0>(input());ti<=t;ti++)
+	// 	cout<<"Case #"<<ti<<": ",
+		solve();
+}
--- src/saved/max_k_sum.cpp
+++ src/saved/max_k_sum.cpp
@@ -1,19 +1,33 @@
 #include "tree/seg/seg.h"
+#include "tree/seg/segdyn.h"
 
 signed main(){
-	int n=10;
-	Seg<MAdd<int>> st0(n),st1(n);
+	// int n=10;
+	// Seg<MAdd<int>> st0(n),st1(n);
+	// func(void,add,int x){
+	// 	st0.upd(x,st0.q(x)+1);
+	// 	st1.upd(x,st1.q(x)+x);
+	// };
+	// func(void,del,int x){
+	// 	st0.upd(x,st0.q(x)-1);
+	// 	st1.upd(x,st1.q(x)-x);
+	// };
+	// func(int,qry,int k){
+	// 	int m=st0.liftRL(lam(acc>=k,int acc,int i));
+	// 	return st1.q(m,n);
+	// };
+	SegDyn<MAdd<int>,lamp(x+y,int x,int y)> st0,st1;
 	func(void,add,int x){
-		st0.upd(x,st0.q(x)+1);
-		st1.upd(x,st1.q(x)+x);
+		st0.upd(x,1);
+		st1.upd(x,x);
 	};
 	func(void,del,int x){
-		st0.upd(x,st0.q(x)-1);
-		st1.upd(x,st1.q(x)-x);
+		st0.upd(x,-1);
+		st1.upd(x,-x);
 	};
 	func(int,qry,int k){
 		int m=st0.liftRL(lam(acc>=k,int acc,int i));
-		return st1.q(m,n);
+		return st1.q(m,inf<int>());
 	};
 	add(3);
 	add(4);
