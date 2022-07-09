1526f28f33e68fa5244fd714e9e04ed73d18b9d4
0xb612f <0xb612f@gmail.com>
2022-07-09 10:57:53 +0900

    Update interface of heapset

--- incl/misc/heapset.h
+++ incl/misc/heapset.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "core/base.h"
 
-template<class T,bool mx=false> struct HeapSet{
-	void add(T x){a.push(mx?-x:x);}
-	void del(T x){b.push(mx?-x:x);}
-	T get(){
-		while(sz(b)&&b.top()==a.top())b.pop(),a.pop();
-		if(a.empty())return mx?-inf<int>():inf<int>();
-		return mx?-a.top():a.top();}
-	int size()const{return sz(a)-sz(b);}
-	PQ<T,greater<>> a,b;
+template<class T, class CMP> struct HeapSet{
+	HeapSet(CMP cmp):a(cmp),b(cmp){}
+	inline void add(T x){a.push(x);}
+	inline void del(T x){b.push(x);}
+	inline T get(){
+		while(sz(b)&&b.top()==a.top())
+			b.pop(),a.pop();
+		return a.top();
+	}
+	inline int size()const{return sz(a)-sz(b);}
+	PQ<T,CMP> a,b;
 };
