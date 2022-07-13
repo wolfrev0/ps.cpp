08aec695d9ac114d449e2b63be8e284b608ac444
0xb612f <0xb612f@gmail.com>
2022-07-14 00:04:42 +0900

    random input generators

---
+++ incl/misc/gen.h
@@ -0,0 +1,105 @@
+#pragma once
+#include "misc/random.h"
+
+namespace gen{
+	Arr<int> seq(int n,int min,int max){
+		Arr<int> a(n);
+		for(auto&i:a)
+			i=rd(min,max+1);
+		return a;
+	}
+
+	Arr<int> seq_sum(int n,int sum,int min){
+		//max=n-min*(n-1)
+		sum-=min*n;//watch out for overflow
+		Arr<int> p;
+		p.emplace_back(0);
+		for(int i=0;i<n-1;i++)
+			p.emplace_back(rd(0,sum+1));
+		p.emplace_back(sum);
+		sort(p.begin(),p.end());
+		Arr<int> arr;
+		for(int i=1;i<sz(p);i++)
+			arr.emplace_back(p[i]-p[i-1]+min);
+		return arr;
+	}
+
+	Arr<int> permu(int n){
+		Arr<int> a(n);
+		iota(a.begin(),a.end(),0);
+		shuffle(a.begin(),a.end());
+		return a;
+	}
+
+	Arr<pint> tree(int n,function<int(int)> fidx=lamp(rd(idx),int idx)){
+		Arr<pint> a;
+		auto b=permu(n);
+		for(int i=1;i<n;i++)
+			a.emplace_back(b[fidx(i)],b[i]);
+		shuffle(a.begin(),a.end());
+		return a;
+	}
+	Arr<pint> star(int n, f64 randomness=0){
+		return tree(n,lam(rdf()<randomness?rd(idx):0,int idx));
+	}
+	Arr<pint> bamboo(int n, f64 randomness=0){
+		return tree(n,lam(rdf()<randomness?rd(idx):idx-1,int idx));
+	}
+
+	template<bool simple=true>
+	Arr<pint> graph(int v,int e){
+		//if not simple, graph can have (self-loop, multi-edge, disconnection)
+		Arr<pint> a;
+		if(v<5000){
+			if(simple){
+				for(int i=0;i<v;i++)
+					for(int j=0;j<i;j++)
+						a.emplace_back(i,j);
+				shuffle(a.begin(),a.end());
+				a.erase(a.begin()+e,a.end());
+			}else{
+				for(int i=0;i<e;i++)
+					a.emplace_back(rd(v),rd(v));
+			}
+		}else{
+			set<pint> z;
+			if(simple){
+				for(auto [x,y]:tree(v))
+					z.emplace(min(x,y),max(x,y));
+			}
+			while(sz(z)<e){
+				int x=rd(v),y=rd(v);
+				if(simple && x==y)
+					continue;
+				z.emplace(min(x,y),max(x,y));
+			}
+			for(auto [x,y]:z)
+				a.emplace_back(rd(2)?mkp(x,y):mkp(y,x));
+		}
+		shuffle(a.begin(),a.end());
+		return a;
+	}
+	Arr<pint> grid(int n,int m){
+		//v=n*m, e=(n-1)*m+n*(m-1)
+		Arr<pint> a;
+		auto b=permu(n*m);
+		for(int i=0;i<n;i++)
+			for(int j=0;j<m-1;j++)
+				a.emplace_back(b[i*m+j], b[i*m+j+1]);
+		for(int j=0;j<m;j++)
+			for(int i=0;i<n-1;i++)
+				a.emplace_back(b[i*m+j], b[(i+1)*m+j]);
+		shuffle(a.begin(),a.end());
+		return a;
+	}
+	Arr<pint> bicross(int v){
+		//e ~= 1.5v
+		Arr<pint> a;
+		auto b=permu(v);
+		for(int i=0;i<v-1;i++)
+			a.emplace_back(b[i],b[i+1]);
+		for(int i=0;i+2<v;i+=2)
+			a.emplace_back(b[i],b[i+2]);
+		return a;
+	}
+}
--- incl/misc/random.h
+++ incl/misc/random.h
@@ -8,16 +8,3 @@ int rd(int e){return rd()%e;}
 int rd(int s,int e){return rd()%(e-s)+s;}
 f64 rdf(){static uniform_real_distribution<f64> dist(0,1);return dist(_rng);}
 void shuffle(auto is,auto ie){shuffle(is,ie,_rng);}
-
-//0이상 sum이하 랜덤. 1이상 필요하면 미리 n만큼 빼두고 구한다음 나중에 +1
-Arr<int> rdarrsum(int n,int sum){
-	Arr<int> p;
-	p.emplace_back(0);
-	for(int i=0;i<n-1;i++)
-		p.emplace_back(rd(0,sum+1));
-	p.emplace_back(sum);
-	sort(p.begin(),p.end());
-	Arr<int> arr;
-	for(int i=1;i<sz(p);i++)
-		arr.emplace_back(p[i]-p[i-1]);
-	return arr;}
