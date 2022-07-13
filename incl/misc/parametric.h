79c3bab15927ae808a2c375c3df000b95b424441
0xb612f <0xb612f@gmail.com>
2022-07-14 00:17:16 +0900

    parametric.h

---
+++ incl/misc/parametric.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "core/base.h"
+
+pint bisect(auto det,int s=-inf<int>(),int e=inf<int>()){
+	bool ds=det(s),de=det(e);
+	if(ds==de)
+		return {ds,de};
+	while(e-s>1){
+		int m=(s+e)/2;
+		if(det(m)==ds) s=m;
+		else e=m;
+	}
+	return {s,e};
+};
+template<auto cmp>
+tint ternary(auto val,int s=-inf<int>(),int e=inf<int>()){
+	auto [rs,re]=bisect(lam(cmp(f(mid),f(mid+1)),int mid),s,e-1);
+	return {rs,re,re+1};
+};
