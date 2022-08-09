7f213d9915cfa392b6495564e80251e04db2f965
0xb612f <0xb612f@gmail.com>
2022-08-10 02:36:30 +0900

    test script update

--- test/stress.sh
+++ test/stress.sh
@@ -2,14 +2,14 @@
 #examples
 #RTE 체크: stress.sh genR mysolR mysolR
 #diff 체크: stress.sh genR mysolD correctR
+
 RED='\033[1;31m'
 CYAN='\033[1;36m'
 GREEN='\033[1;32m'
 NONE='\033[0m'
-set -e
-trap "echo -e '${RED}FAIL${NONE}'" ERR
-for((i=0;;++i));do
-	$1>test/tc/in
-	diff <($2<test/tc/in) <($3<test/tc/in) -BZ
-	echo -e "${GREEN}OK, $i tests${NONE}"
+
+i=0
+while $1>test/tc/in && diff <($2<test/tc/in) <($3<test/tc/in) -BZ; do
+	echo -e "${GREEN}OK, $((++i)) tests${NONE}"
 done
+echo -e "${RED}FAIL${NONE}"
--- test/tc.sh
+++ test/tc.sh
@@ -4,10 +4,22 @@ GREEN='\033[1;32m'
 NONE='\033[0m'
 shopt -s nullglob
 # set -e
-trap "echo -e '${RED}FAIL${NONE}'" ERR
+# trap "echo -e '${RED}FAIL${NONE}'" ERR
+test_cnt=$(find test/tc/ -name "*.in" -printf '.' | wc -m)
+succ=0
+fail=0
 for i in test/tc/*.in;
 do
-	echo "########## "${i%.*}" ##########"
-	diff <($1<$i) ${i%.*}.ans -BZ
+	echo "Running "${i%.*}" ("$(( ($succ+$fail)*100/$test_cnt ))"%)"
+	if diff <($1<$i) ${i%.*}.ans -BZ > /dev/null 2>&1; then
+		succ=$((succ+1))
+	else
+		fail=$(($fail+1))
+		fail_list+=$i", ";
+	fi
 done
-echo -e "${GREEN}OK${NONE}"
+echo -e "${GREEN}Ok: $succ tests${NONE}"
+if (( $fail )); then
+	echo -e "${RED}Fail: $fail tests. [$fail_list] ${NONE}"
+	exit 1;
+fi
