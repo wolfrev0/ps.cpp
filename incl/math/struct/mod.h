bb7f4438325cdf1f0d77abd39f6e964c14f47a28
0xb612f <0xb612f@gmail.com>
2023-03-23 00:01:54 +0900

    mod optimization

--- incl/math/struct/mod.h
+++ incl/math/struct/mod.h
@@ -3,43 +3,36 @@
 
 //Mod객체로 DP할거면 반드시 optional 사용해야함. nan같은거 사용x
 template<int m> struct Mod{
-	Mod(i64 n=0):n(n<m?n:n-m){if(n<0||n>=m)n=(n%m+m)%m;}
+	static_assert(m<numeric_limits<int>::max()/2);
+	Mod(int n=0):n(n){assert(0<=n && n<m);}
 	explicit operator int()const{return n;}
 	explicit operator bool()const{return !!n;}
 
-	Mod operator-()const{return -n;}
-	Mod operator+(const Mod& b)const{return n+b.n;}
-	Mod operator-(const Mod& b)const{return n-b.n+m;}
-	Mod operator*(const Mod& b)const{return n*b.n;}
+	Mod operator-()const{return n?m-n:0;}
+	Mod operator+(const Mod& b)const{return n+b.n >= m ? n+b.n-m : n+b.n;}
+	Mod operator-(const Mod& b)const{return n-b.n < 0 ? n-b.n+m : n-b.n;}
+	Mod operator*(const Mod& b)const{return i64(n)*b.n%m;}
 	Mod operator/(const Mod& b)const{return *this*b.inv();}
 	Mod operator+=(const Mod& b){return *this=*this+b;}
 	Mod operator-=(const Mod& b){return *this=*this-b;}
 	Mod operator*=(const Mod& b){return *this=*this*b;}
 	Mod operator/=(const Mod& b){return *this=*this/b;}
-	Mod pow(int a)const{ if(!a)return 1;auto z=pow(a/2);return z*z*(a%2?*this:1);}
+	Mod pow(int a)const{ if(!a)return 1;auto z=pow(a>>1);return z*z*(a&1?*this:1);}
 	//Mod inv()const{return this->pow(m-2);}
-	Mod inv()const{auto [g,x,y]=xgcd(n, m);assert(g==1);while(x<0)x+=m;return x;}
-
-	Mod& operator++(){*this+=1ll;return *this;}
-	Mod& operator--(){*this-=1ll;return *this;}
-	Mod operator++(signed){auto ret=*this;++*this;return ret;}
-	Mod operator--(signed){auto ret=*this;--*this;return ret;}
+	Mod inv()const{auto [g,x,y]=xgcd(n, m);assert(g==1);return (x%m+m)%m;}
 
 	bool operator==(const Mod& r)const{return n==r.n;}
 	bool operator!=(const Mod& r)const{return !(*this==r);}
 	bool operator<(const Mod& r)const{return n<r.n;}
 
-	i64 n;
-	tint xgcd(i64 a, i64 b)const{
+	int n;
+	tint xgcd(int a, int b)const{
 		if(!b)return{a,1,0};
 		auto [g,x,y]=xgcd(b,a%b);
 		return{g,y,x-(a/b)*y};
 	}
 };
 template<int m> ostream& operator<<(ostream& s,const Mod<m>& n){return s<<n.n;}
-template<int m> Mod<m> operator+(int a,const Mod<m>& b){return b+a;}
-template<int m> Mod<m> operator-(int a,const Mod<m>& b){return -b+a;}
-template<int m> Mod<m> operator*(int a,const Mod<m>& b){return b*a;}
 
 // TODO: Optimize for special modes
 // template<> struct Mod<(1<<16)>{
