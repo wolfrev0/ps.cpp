b118556e02040352574fbe6a5275c0fb6cc6872f
0xb612f <0xb612f@gmail.com>
2023-03-27 14:45:30 +0900

    compile.sh set -e

--- test/compile.sh
+++ test/compile.sh
@@ -1,8 +1,9 @@
+set -e
 for i in $(find incl -name '*.h' ! -wholename 'incl/testlib.h')
 do
-	echo '#include "'${i#*incl/}'"' >> _tmp_compile_.cpp
+	echo '#include "'${i#*incl/}'"' >> res/_tmp_compile_.cpp
 done
-echo 'signed main(){}' >> _tmp_compile_.cpp
-tool/build.sh _tmp_compile_.cpp D
-tool/build.sh _tmp_compile_.cpp R
-rm _tmp_compile_.cpp
+echo 'signed main(){}' >> res/_tmp_compile_.cpp
+tool/build.sh res/_tmp_compile_.cpp D
+tool/build.sh res/_tmp_compile_.cpp R
+rm res/_tmp_compile_.cpp
