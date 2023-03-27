d46d673e9e5850eeeb598d85c7590e9bbbce23fe
0xb612f <0xb612f@gmail.com>
2023-03-27 15:11:09 +0900

    [arr.h] remove i>=n case

--- incl/core/arr.h
+++ incl/core/arr.h
@@ -2,7 +2,6 @@
 #include "core/std.h"
 #include "core/config.h"
 #include "core/func1.h"
-#include "core/debug.h"
 
 template<class T, class P=vector<T>>
 struct Arr:public P{
@@ -11,22 +10,8 @@ struct Arr:public P{
 	explicit Arr(signed n,T init):P(n,init){}
 	Arr(initializer_list<T>il):P(il){}
 	Arr(auto its, auto ite):P(its,ite){}
-	inline T& operator[](signed i){
-		int n=sz(*this);
-		if(i<0)
-			i+=n,dbg1("Neg Index Found");
-		if(i>=n)
-			i-=n,dbg1("Over Index Found");
-		return P::operator[](i);
-	}
-	const T& operator[](signed i)const{	
-		int n=sz(*this);
-		if(i<0)
-			i+=n,dbg1("Neg Index Found");
-		if(i>=n)
-			i-=n,dbg1("Over Index Found");
-		return P::operator[](i);
-	}
+	inline T& operator[](signed i){return P::operator[](i<0?i+sz(*this):i);}
+	const T& operator[](signed i)const{return P::operator[](i<0?i+sz(*this):i);}
 	T& at(signed i){return *this[i];}
 	const T& at(signed i)const{return *this[i];}
 };
