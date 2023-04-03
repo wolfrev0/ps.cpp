5cef4e0ff142f71041a52a2ee61ed417aee5024a
wolfrev0 <wolfrevo1337@gmail.com>
2023-04-03 13:39:18 +0900

    isqrt bugfix

--- incl/core/func1.h
+++ incl/core/func1.h
@@ -18,8 +18,8 @@ int fdiv(int a,int b){if(b<0)a=-a,b=-b;return (a>0)?a/b:(a-b+1)/b;}
 int cdiv(int a,int b){if(b<0)a=-a,b=-b;return (a>0)?(a+b-1)/b:a/b;}
 i64 flg2(u64 x){return 63-countl_zero(x);}
 i64 clg2(u64 x){return x-1==0?0:64-countl_zero(x-1);}
-int fsqrt(i64 n) {i64 i=1;while(i*i<=n)i++;return i-1;}
-int csqrt(i64 n) {i64 i=1;while(i*i<n)i++;return i;}
+int fsqrt(i64 n) {i64 i=0;while(i*i<=n)i++;return i-1;}
+int csqrt(i64 n) {i64 i=0;while(i*i<n)i++;return i;}
 template<class T>T sq(T x){return x*x;}
 template<class T>constexpr T inf(){return numeric_limits<T>::max()/2;}
 template<class T> [[deprecated("use optional")]] constexpr T nan(){return inf<T>()+1;}
@@ -36,13 +36,6 @@ template<class...A>ostream& osprint(ostream& os, A...a){return ((os<<a),...);}
 #define println(...) osprint(cout,__VA_ARGS__,'\n')
 template<class T,class U>bool assmin(T& a,U&& b){return a>b?a=b,true:false;}
 template<class T,class U>bool assmax(T& a,U&& b){return a<b?a=b,true:false;}
-void MUST(bool expr){
-#if DEBUG
-	#include <csignal>
-	if(!expr)
-		raise(SIGINT);
-#endif
-}
 #define ARG(...) __VA_ARGS__
 #define func(RetT,fname,...) function<RetT(__VA_ARGS__)> fname=[&](__VA_ARGS__)->RetT
 #define lam(expr,...) [&](__VA_ARGS__){return expr;}
