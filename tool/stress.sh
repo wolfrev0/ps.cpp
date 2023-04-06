965a9ab822e6bafe5c657dc6f33ac3292fbc7242
wolfrev0 <wolfrevo1337@gmail.com>
2023-04-06 18:56:25 +0900

    stress.sh check segfault

--- tool/stress.sh
+++ tool/stress.sh
@@ -9,7 +9,8 @@ GREEN='\033[1;32m'
 NONE='\033[0m'
 
 i=0
-while $1>test/in && diff <($2<test/in) <($3<test/in) -BZ; do
+
+while $1>test/in && $2<test/in>/tmp/__stress_1.tmp && $3<test/in>/tmp/__stress_2.tmp && diff /tmp/__stress_1.tmp /tmp/__stress_2.tmp -BZ; do
 	echo -e "${GREEN}OK, $((++i)) tests${NONE}"
 done
 echo -e "${RED}FAIL${NONE}"
