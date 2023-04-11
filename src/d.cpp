2a1ca3cd37be225f94b1bad023a6ce1f66c4a7e8
wolfrev0 <wolfrevo1337@gmail.com>
2023-04-11 13:57:42 +0900

    update template code

--- src/d.cpp
+++ src/d.cpp
@@ -1,16 +1,11 @@
 #include "core/base.h"
-#define CHECK_INPUT 1
-#define TC 1//get<0>(input())
-#define TC_OUT_PREFIX ""//"Case #",ti,": "
 signed main(){
 	void solve();
-	for(int ti=1,t=TC;ti<=t;ti++)
-		print(TC_OUT_PREFIX),
+	// for(int ti=1,t=get<0>(input());ti<=t;ti++)
+		// print("Case #",ti,": "),
 		solve();
-#if CHECK_INPUT
 	assert(cin.get()=='\n');
 	assert(cin.get()==EOF);
-#endif
 }
 void solve(){
 }
