53fd8d51c8d0916ab621a971cbd52ac6b420b6e7
wolfrev0 <wolfrevo1337@gmail.com>
2023-06-09 00:42:35 +0900

    Mod optimization

--- incl/math/struct/mod.h
+++ incl/math/struct/mod.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "core/base.h"
 
-//Mod객체로 DP할거면 반드시 optional 사용해야함. nan같은거 사용x
-template<int m> struct Mod{
-	static_assert(m<numeric_limits<int>::max()/2);
-	// Mod(int n=0):n(n){assert(0<=n && n<m);}
-	Mod(int n=0):n((n%m+m)%m){}
-	explicit operator int()const{return n;}
+using ModT=signed;
+template<ModT m> struct Mod{
+	static_assert(m<numeric_limits<ModT>::max()/2);
+	// Mod(ModT n=0):n(n){assert(0<=n && n<m);}
+	Mod(ModT n=0):n((n%m+m)%m){}
+	explicit operator ModT()const{return n;}
+	explicit operator u64()const{return n;}
 	explicit operator bool()const{return !!n;}
 
 	Mod operator-()const{return n?m-n:0;}
@@ -18,7 +19,7 @@ template<int m> struct Mod{
 	Mod operator-=(const Mod& b){return *this=*this-b;}
 	Mod operator*=(const Mod& b){return *this=*this*b;}
 	Mod operator/=(const Mod& b){return *this=*this/b;}
-	Mod pow(int a)const{ if(!a)return 1;auto z=pow(a>>1);return z*z*(a&1?*this:1);}
+	Mod pow(ModT a)const{ if(!a)return 1;auto z=pow(a>>1);return z*z*(a&1?*this:1);}
 	//Mod inv()const{return this->pow(m-2);}
 	Mod inv()const{auto [g,x,y]=xgcd(n, m);assert(g==1);return (x%m+m)%m;}
 
@@ -26,13 +27,14 @@ template<int m> struct Mod{
 	bool operator!=(const Mod& r)const{return !(*this==r);}
 	bool operator<(const Mod& r)const{return n<r.n;}
 
-	int n;
-	tint xgcd(int a, int b)const{
+	ModT n;
+	tint xgcd(ModT a, ModT b)const{
 		if(!b)return{a,1,0};
 		auto [g,x,y]=xgcd(b,a%b);
 		return{g,y,x-(a/b)*y};
 	}
 };
-template<int m> ostream& operator<<(ostream& s,const Mod<m>& n){return osprint(s,n.n);}
+template<ModT m> istream& operator>>(istream& s,Mod<m>& n){ n=input1(s); return s;}
+template<ModT m> ostream& operator<<(ostream& s,const Mod<m>& n){return osprint(s,n.n);}
 
 //TODO: dynamic modint (barrett, montgomery)
