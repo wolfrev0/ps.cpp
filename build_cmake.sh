30d01934aa611c25195d2f91037eb5dcd03827f3
wolfrev0 <wolfrevo1337@gmail.com>
2023-06-13 03:38:32 +0900

    CMake update

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,6 +1,9 @@
+set -e
+
 rm -rf build
 mkdir -p build
-cd build
-cmake ..
+pushd build
+cmake .. -DSOURCE_PATH=../src/main.cpp -DCMAKE_BUILD_TYPE=Release
 cmake --build .
-./ps.cpp
+popd
+build/ps.cpp < test/in
