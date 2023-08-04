41d397dc125cd2ef275c18388a3ce4e5287d6c27
tuxedcat <tuxedcat@gmail.com>
2023-08-04 23:01:08 +0900

    kmp improve

--- incl/str/match.h
+++ incl/str/match.h
@@ -13,14 +13,8 @@ template<class T> Arr<int> failure_function(const Str<T> &p) {
 
 // return: partial matched length of that index
 template<class T> Arr<int> kmp(const Str<T> &s, const Str<T> &p) {
-	auto ff = failure_function(p);
-	Arr<int> ret(sz(s));
-	for(int i = 0, j = 0; j < sz(s); j++) {
-		while(i > 0 and s[j] != p[i]) i = ff[i - 1];
-		if(s[j] == p[i]) ret[j] = ++i;
-		if(i == sz(p)) i = ff[i - 1];
-	}
-	return ret;
+	auto ff = failure_function(p+Str<T>(1,inf<T>()*2)+s);
+	return {ff.begin()+sz(p)+1,ff.end()};
 }
 
 //z[i]=match length of a[0,n-1] and a[i,n-1]
