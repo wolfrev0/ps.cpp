b46ae7053a61fa7429551a694d6acad740400ee3
tuxedcat <tuxedcat@gmail.com>
2023-08-27 07:55:05 +0900

    knapsack

--- incl/dp/knapsack.h
+++ incl/dp/knapsack.h
@@ -33,3 +33,30 @@ Bitset subset_sum_dyn(const Arr<int>& wa){
 	}
 	return dp;
 }
+//a[i].fi=무게, a[i].se=가치, cap
+//cap이하로 물건을 담을때 최대 가치
+int knapsack(const Arr<pint>& a, int cap){
+	auto n=sz(a);
+	auto dp=ARR(n,cap+1,-1ll);
+	func(int,f,int idx,int cap){
+		if(cap<0) return -inf<int>();
+		if(idx==n) return 0;
+		auto&ret=dp[idx][cap];
+		if(~ret)return ret;
+		return ret=max(f(idx+1,cap),f(idx+1,cap-a[idx].fi)+a[idx].se);
+	};
+	return f(0,cap);
+}
+//a[i].fi=무게, a[i].se=가치, val
+//val이상의 가치를 챙기기 위한 최소 무게
+int knapsack2(const Arr<pint>& a, int val){
+	auto n=sz(a);
+	Arr<pint> b;
+	int vsum=0,wsum=0;
+	for(auto [w,v]:a){
+		b.emplace_back(v,w);
+		vsum+=v;
+		wsum+=w;
+	}
+	return wsum-knapsack(b,vsum-val);
+}
