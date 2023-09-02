140343c40b153c8803ab32887340b8e5fe2fce66
tuxedcat <tuxedcat@gmail.com>
2023-09-02 18:06:53 +0900

    signed to i32

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "core/base.h"
-signed main(){
+i32 main(){
 	void solve();
 	// for(int ti=1,t=input1();ti<=t;ti++)
 	// 	print("Case #",ti,'\n'),
 		solve();
-	for(signed c;(c=cin.get())!=EOF;)
+	for(i32 c;(c=cin.get())!=EOF;)
 		assert(c==' '||c=='\n');
 }
 void solve(){
