1c370f42a444b1cf8aca1b42bcc5f8b729dc81c9
wolfrev0 <wolfrevo1337@gmail.com>
2024-12-27 21:24:22 +0900

    update comments in submit.cpp

--- tool/build_cmake.sh
+++ tool/build_cmake.sh
@@ -34,17 +34,16 @@ function f(){
 awk '//' $(f|tac) | grep -Ev '#include *"|#pragma once' > build/submit.cpp
 cat build/submit.cpp | head -n 11 > build/build0.tmp
 cat build/submit.cpp | tail -n +12 > build/build1.tmp
-echo "//[Author]   tuxedcat" > build/submit.cpp
-echo "//[Date]     $(date "+%Y.%m.%d %H:%M:%S %Z")" >> build/submit.cpp
-echo "//[File]     $src" >> build/submit.cpp
-echo "//[Library]  https://github.com/tuxedcat/ps.cpp" >> build/submit.cpp
-echo "//[Revision] $(git rev-parse HEAD)" >> build/submit.cpp
+rm build/submit.cpp
+echo "// [Library]  https://github.com/wolfrev0/ps.cpp/commit/$(git rev-parse HEAD)" >> build/submit.cpp
+echo "// [Date]     $(date "+%Y.%m.%d %H:%M:%S %Z")" >> build/submit.cpp
+echo "/* [ORIGINAL_SOURCE_CODE]" >> build/submit.cpp
+cat $src >> build/submit.cpp
+echo "*/" >> build/submit.cpp
+
 # echo "#pragma GCC optimize(\"O3\")" >> build/submit.cpp
 # echo "#pragma GCC target(\"avx2\")" >> build/submit.cpp
 # echo "#define NDEBUG" >> build/submit.cpp
-echo "/* ORIGINAL_MAIN_SOURCE" >> build/submit.cpp
-cat $src >> build/submit.cpp
-echo "*/" >> build/submit.cpp
 cat build/build0.tmp >> build/submit.cpp
 cpp -P build/build1.tmp | clang-format -style=Google >> build/submit.cpp
 
